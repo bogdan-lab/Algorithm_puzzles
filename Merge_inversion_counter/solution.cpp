428bde37172ba54b5a7a94a317bba2f56c06dfe0
bogdan <lakatosh@phystech.edu>
2020-09-14 22:40:13 +0300

    Added boost random for generating random arrays for testing in inversion counter Boost can bring A LOT OF CONVERSION WARNINGS...

--- Merge_inversion_counter/solution.cpp
+++ Merge_inversion_counter/solution.cpp
@@ -4,6 +4,9 @@
 #include <iostream>
 #include <vector>
 #include <assert.h>
+#include <ctime>
+#include "boost/random/mersenne_twister.hpp"
+#include "boost/random/uniform_int_distribution.hpp"
 
 void swap(int& lhs, int& rhs){
     int tmp = rhs;
@@ -42,7 +45,7 @@ size_t merge(std::vector<int>::iterator begin, std::vector<int>::iterator mid, s
 
 
 size_t calculate_inversions(std::vector<int>::iterator begin, std::vector<int>::iterator end){
-    if(end-begin==1){
+    if(end-begin<=1){
         return 0;
     }
     size_t count = 0;
@@ -54,22 +57,70 @@ size_t calculate_inversions(std::vector<int>::iterator begin, std::vector<int>::
 }
 
 
+size_t brut_force_solution(const std::vector<int>& vec){
+    if(vec.size()==0){return 0;}
+    size_t count = 0;
+    for(size_t i=0; i<vec.size()-1; i++){
+        for(size_t j=i+1; j<vec.size(); j++){
+            if(vec[i]>vec[j]){
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+std::ostream& operator<<(std::ostream& out, const std::vector<int>& vec){
+    if(vec.size()==0){
+        out << "[]";
+        return out;
+    }
+    out << "[ ";
+    for(size_t i=0; i<vec.size()-1; i++){
+        out << vec[i] << ", ";
+    }
+    out << vec[vec.size()-1] << " ]";
+    return out;
+}
 
 int main(){
     {
         std::vector<int> test = {2,3,8,6,1};
-        size_t res = calculate_inversions(test.begin(), test.end());
-        assert(res==5);
+        size_t res2 = brut_force_solution(test);
+        size_t res1 = calculate_inversions(test.begin(), test.end());
+        assert(res1==5);
+        assert(res2==5);
     }
     {
         std::vector<int> test={5, 4, 3, 2, 1};
-        size_t res = calculate_inversions(test.begin(), test.end());
-        assert(res==10);
+        size_t res2 = brut_force_solution(test);
+        size_t res1 = calculate_inversions(test.begin(), test.end());
+        assert(res1==10);
+        assert(res2==10);
     }
     {
         std::vector<int> test={1,2,3,4,5};
-        size_t res = calculate_inversions(test.begin(), test.end());
-        assert(res==0);
+        size_t res2 = brut_force_solution(test);
+        size_t res1 = calculate_inversions(test.begin(), test.end());
+        assert(res1==0);
+        assert(res2==0);
+    }
+    boost::random::mt19937 gen(static_cast<uint>(std::time(0)));
+    boost::random::uniform_int_distribution<> num_dist(-100, 100);
+    boost::random::uniform_int_distribution<> len_dist(0, 20);
+    std::vector<int> test_vec;
+    for(size_t N=0; N<20; N++){
+        size_t len = static_cast<size_t>(len_dist(gen));
+        test_vec.reserve(len);
+        for(size_t i=0; i<len; i++){
+            test_vec.push_back(num_dist(gen));
+        }
+        std::cerr << "Testing vector: " << test_vec << "\n";
+        size_t res1 = brut_force_solution(test_vec);
+        size_t res2 = calculate_inversions(test_vec.begin(), test_vec.end());
+        assert(res1==res2);
+        std::cerr << "Counted " << res1 << " inversions\n";
+        test_vec.clear();
     }
     return 0;
 }
