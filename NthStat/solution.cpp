ccb562f528068c7ab15de940f95ee11cc606daa6
bogdan <lakatosh@phystech.edu>
2020-12-01 22:19:26 +0300

    working quick implementation

--- NthStat/solution.cpp
+++ NthStat/solution.cpp
@@ -47,6 +47,39 @@ int get_nth_heap(std::vector<int> vec, const size_t el_num){
     return pop_max(vec);
 }
 
+
+size_t partition(std::vector<int>& vec, const size_t start, const size_t end){
+    if(start==end) return start;
+    auto l_idx = start;
+    auto r_idx = end-1;
+    while(true){
+        while(l_idx<end && vec[l_idx]>=vec[end]) l_idx++;
+        while(r_idx>start && vec[r_idx]<vec[end]) r_idx--;
+        if(l_idx>=r_idx) break;
+        std::swap(vec[l_idx], vec[r_idx]);
+        l_idx++;
+        r_idx--;
+    }
+    std::swap(vec[end], vec[l_idx]);
+    return l_idx;
+}
+
+
+int get_nth_quick(std::vector<int> vec, const size_t el_num){
+    size_t start = 0;
+    auto end = vec.size()-1;
+    auto res = partition(vec, start, end);
+    while(res!=el_num){
+        if(res>el_num){end = res-1;}
+        else{start = res+1;}
+        res = partition(vec, start, end);
+    }
+    return vec[res];
+}
+
+
+
+
 TEST_CASE("Heap itself test", "[heap]"){
     std::vector<int> tst = {12,54,7,213,9,0,12,6,95,124,1};
     build_heap(tst);
@@ -71,4 +104,20 @@ TEST_CASE("Heap nth test", "[heap]"){
     CHECK(get_nth_heap(tst2, 8) == 6);
     CHECK(get_nth_heap(tst2, 9) == 5);
     CHECK(get_nth_heap(tst2, 10) == 3);
+}
+
+TEST_CASE("Quick sort nth", "[quick]"){
+    std::vector<int> tst2 = {12,6,87,98,5,3,9,45,6,8,79};
+    CHECK(get_nth_quick(tst2, 0) == 98);
+    CHECK(get_nth_quick(tst2, 1) == 87);
+    CHECK(get_nth_quick(tst2, 2) == 79);
+    CHECK(get_nth_quick(tst2, 3) == 45);
+    CHECK(get_nth_quick(tst2, 4) == 12);
+    CHECK(get_nth_quick(tst2, 5) == 9);
+    CHECK(get_nth_quick(tst2, 6) == 8);
+    CHECK(get_nth_quick(tst2, 7) == 6);
+    CHECK(get_nth_quick(tst2, 8) == 6);
+    CHECK(get_nth_quick(tst2, 9) == 5);
+    CHECK(get_nth_quick(tst2, 10) == 3);
+
 }
