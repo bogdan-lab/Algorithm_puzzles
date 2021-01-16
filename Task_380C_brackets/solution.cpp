de7e01754ceb4248d0c82a32191c723c92a303f2
bogdan <lakatosh@phystech.edu>
2021-01-16 19:53:27 +0300

    Fixed bug. Time problems... -> do segment tree for brackets!

--- Task_380C_brackets/solution.cpp
+++ Task_380C_brackets/solution.cpp
@@ -13,19 +13,10 @@ size_t get_max_length(const std::string& brackets, const size_t start,
                       const size_t finish);
 
 
-
+void run_tests();
 int main(){
     std::ios_base::sync_with_stdio (false);
-    std::stringstream ss;
-    ss << "())(())(())(\n";
-    ss << R"(7
-          1 1
-          2 3
-          1 2
-          1 12
-          8 12
-          5 11
-          2 10)";
+    std::cin.tie(nullptr);
     solve(std::cin);
     return 0;
 }
@@ -39,16 +30,16 @@ void solve(std::istream &input){
     size_t m;
     input >> m;
     std::vector<size_t> bnds;
-    bnds.reserve(m);
+    bnds.reserve(2*m);
     std::copy_n(std::istream_iterator<size_t>(input), 2*m, std::back_inserter(bnds));
-    std::for_each(bnds.begin(), bnds.end(), [](size_t& el){return el-1;});
-    process_requests(std::move(brackets), bnds);
+    std::for_each(bnds.begin(), bnds.end(), [](size_t& el){el--;});
+    process_requests(brackets, bnds);
 }
 
 
 void process_requests(const std::string& brackets, const std::vector<size_t>& bnds){
     for(size_t i=0; i<bnds.size()/2; i++){
-        std::cout << get_max_length(brackets, bnds[2*i]-1, bnds[2*i+1]-1) << '\n';
+        std::cout << get_max_length(brackets, bnds[2*i], bnds[2*i+1]) << '\n';
     }
 }
 
@@ -63,7 +54,31 @@ size_t get_max_length(const std::string& brackets, const size_t start,
     size_t count_left = 0;
     for(size_t i=begin; i<end+1; i++){
         count_right += brackets[i]=='(';
-        count_left += brackets[i]==')';
+        count_left += brackets[i]==')' && count_right>count_left;
     }
-    return 2*std::min(count_right, count_left);
+    return 2*count_left;
+}
+
+
+void run_tests(){
+    {
+        std::stringstream ss;
+        ss << "())(())(())(\n";
+        ss << R"(7
+              1 1
+              2 3
+              1 2
+              1 12
+              8 12
+              5 11
+              2 10)";
+        solve(ss);
+    }
+    {
+        std::stringstream ss;
+        ss << "())(()\n";
+        ss<<R"(1
+            1 6)";
+        solve(ss);
+    }
 }
