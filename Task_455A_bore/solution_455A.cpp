1654a98cad775b0c901b179d4910f82f9c0b58e4
bogdan <lakatosh@phystech.edu>
2021-01-17 20:49:56 +0300

    Now it is solved

--- Task_455A_bore/solution_455A.cpp
+++ Task_455A_bore/solution_455A.cpp
@@ -6,102 +6,68 @@
 #include <iterator>
 #include <numeric>
 
-size_t solve(std::istream& input = std::cin);
+
+int64_t solve(std::istream& input = std::cin);
 void run_tests();
-size_t extract_points_with_min_loss(std::vector<size_t>& points, size_t& zero_counter);
-size_t get_best_points_idx(const std::vector<size_t>& points);
 
 int main(){
-    run_tests();
-    //std::cout << solve();
+    //run_tests();
+    std::cout << solve();
     return 0;
 }
 
 
 void run_tests(){
-    /*{
+    {
         std::stringstream ss;
         ss << R"(2
               1 2)";
-        size_t answer = solve(ss);
+        int64_t answer = solve(ss);
         assert(answer == 2);
     }
     {
         std::stringstream ss;
         ss << R"(3
               1 2 3)";
-        size_t answer = solve(ss);
+        int64_t answer = solve(ss);
         assert(answer == 4);
     }
     {
         std::stringstream ss;
         ss << R"(9
              1 2 1 3 2 2 2 2 3)";
-        size_t answer = solve(ss);
+        int64_t answer = solve(ss);
         assert(answer == 10);
-    }*/
+    }
     {
         std::stringstream ss;
         ss << R"(100
               6 6 8 9 7 9 6 9 5 7 7 4 5 3 9 1 10 3 4 5 8 9 6 5 6 4 10 9 1 4 1 7 1 4 9 10 8 2 9 9 10 5 8 9 5 6 8 7 2 8 7 6 2 6 10 8 6 2 5 5 3 2 8 8 5 3 6 2 1 4 7 2 7 3 7 4 10 10 7 5 4 7 5 10 7 1 1 10 7 7 7 2 3 4 2 8 4 7 4 4)";
-        size_t answer = solve(ss);
-        std::cerr << "Answer = " << answer << '\n';
+        int64_t answer = solve(ss);
         assert(answer == 296);
+    }
+    {
+        std::stringstream ss;
+        ss << 100'000 << '\n';
+        for(int64_t i=0; i<100'000; i++) ss << 100'000 << ' ';
+        int64_t answer = solve(ss);
+        assert(answer == 10'000'000'000);
     }
 }
 
-size_t solve(std::istream &input){
+int64_t solve(std::istream &input){
     size_t n;
     input >> n;
     std::vector<size_t> data;
     data.reserve(n);
     std::copy_n(std::istream_iterator<size_t>(input), n, std::back_inserter(data));
     size_t max_val = *(std::max_element(data.cbegin(), data.cend()));
-    std::vector<size_t> points(max_val+1, 0);
+    std::vector<int64_t> points(max_val+1, 0);
     for(const auto el : data){
-        points[el]+=el;
+        points[el]+=static_cast<int64_t>(el);
     }
-    size_t zero_counter = static_cast<size_t>(std::count(points.begin(), points.end(), 0));
-    size_t max_pts = 0;
-    while(zero_counter<points.size()){
-        max_pts += extract_points_with_min_loss(points, zero_counter);
+    for(size_t i=2; i<points.size(); i++){
+        points[i] = std::max(points[i-1], points[i-2]+points[i]);
     }
-    return max_pts;
-}
-
-size_t extract_points_with_min_loss(std::vector<size_t>& points, size_t& zero_counter){
-    size_t opt_pts_idx = get_best_points_idx(points);
-    size_t tmp = points[opt_pts_idx];
-    points[opt_pts_idx] = 0;
-    zero_counter++;
-    if(opt_pts_idx>=1){
-        zero_counter +=  points[opt_pts_idx-1]!=0;
-        points[opt_pts_idx-1] = 0;
-    }
-    if(opt_pts_idx<points.size()-1){
-        zero_counter += points[opt_pts_idx+1]!=0;
-        points[opt_pts_idx+1] = 0;
-    }
-    return tmp;
-}
-
-
-size_t get_best_points_idx(const std::vector<size_t> &points){
-    //auto max_el_it = std::max_element(points.cbegin(), points.cend());
-    //return static_cast<size_t>(max_el_it - points.cbegin());
-
-    size_t min_loss = std::numeric_limits<size_t>::max();
-    size_t opt_pts_idx = 0;
-    for(size_t i=0; i<points.size(); i++){
-        if(points[i]!=0){
-            size_t lhs_loss = i==0 ? 0 : points[i-1];
-            size_t rhs_loss = i==points.size()-1 ? 0 : points[i+1];
-            size_t curr_loss = lhs_loss + rhs_loss;
-            if(min_loss>curr_loss){
-                opt_pts_idx = i;
-                min_loss = curr_loss;
-            }
-        }
-    }
-    return opt_pts_idx;
+    return points.back();
 }
