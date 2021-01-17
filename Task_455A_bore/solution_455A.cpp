e6f498b3c3aedd67bafaf1058cb13a5c6f95c061
bogdan <lakatosh@phystech.edu>
2021-01-17 18:08:22 +0300

    Bad test 9 think about strategy of peaking points

--- Task_455A_bore/solution_455A.cpp
+++ Task_455A_bore/solution_455A.cpp
@@ -9,6 +9,7 @@
 size_t solve(std::istream& input = std::cin);
 void run_tests();
 size_t extract_points_with_min_loss(std::vector<size_t>& points, size_t& zero_counter);
+size_t get_best_points_idx(const std::vector<size_t>& points);
 
 int main(){
     run_tests();
@@ -44,7 +45,8 @@ void run_tests(){
         ss << R"(100
               6 6 8 9 7 9 6 9 5 7 7 4 5 3 9 1 10 3 4 5 8 9 6 5 6 4 10 9 1 4 1 7 1 4 9 10 8 2 9 9 10 5 8 9 5 6 8 7 2 8 7 6 2 6 10 8 6 2 5 5 3 2 8 8 5 3 6 2 1 4 7 2 7 3 7 4 10 10 7 5 4 7 5 10 7 1 1 10 7 7 7 2 3 4 2 8 4 7 4 4)";
         size_t answer = solve(ss);
-        assert(answer == 296); //296
+        std::cerr << "Answer = " << answer << '\n';
+        assert(answer == 296);
     }
 }
 
@@ -68,19 +70,7 @@ size_t solve(std::istream &input){
 }
 
 size_t extract_points_with_min_loss(std::vector<size_t>& points, size_t& zero_counter){
-    size_t min_loss = std::numeric_limits<size_t>::max();
-    size_t opt_pts_idx = 0;
-    for(size_t i=0; i<points.size(); i++){
-        if(points[i]!=0){
-            size_t lhs_loss = i==0 ? 0 : points[i-1];
-            size_t rhs_loss = i==points.size()-1 ? 0 : points[i+1];
-            size_t curr_loss = lhs_loss + rhs_loss;
-            if(min_loss>curr_loss){
-                opt_pts_idx = i;
-                min_loss = curr_loss;
-            }
-        }
-    }
+    size_t opt_pts_idx = get_best_points_idx(points);
     size_t tmp = points[opt_pts_idx];
     points[opt_pts_idx] = 0;
     zero_counter++;
@@ -95,3 +85,23 @@ size_t extract_points_with_min_loss(std::vector<size_t>& points, size_t& zero_co
     return tmp;
 }
 
+
+size_t get_best_points_idx(const std::vector<size_t> &points){
+    //auto max_el_it = std::max_element(points.cbegin(), points.cend());
+    //return static_cast<size_t>(max_el_it - points.cbegin());
+
+    size_t min_loss = std::numeric_limits<size_t>::max();
+    size_t opt_pts_idx = 0;
+    for(size_t i=0; i<points.size(); i++){
+        if(points[i]!=0){
+            size_t lhs_loss = i==0 ? 0 : points[i-1];
+            size_t rhs_loss = i==points.size()-1 ? 0 : points[i+1];
+            size_t curr_loss = lhs_loss + rhs_loss;
+            if(min_loss>curr_loss){
+                opt_pts_idx = i;
+                min_loss = curr_loss;
+            }
+        }
+    }
+    return opt_pts_idx;
+}
