0d4d42e61d88d4dc656ffa8ab189b59ba6fa117b
bogdan <lakatosh@phystech.edu>
2021-01-23 17:47:52 +0300

    Done

---
+++ Simple_stuff/task_1339A.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+
+typedef unsigned long long ull;
+
+ull power(const unsigned val, const unsigned p){
+    ull res = 1;
+    unsigned idx = p;
+    while(idx--){
+        res *= static_cast<ull>(val);
+    }
+    return res;
+}
+
+int main(){
+    unsigned t;
+    std::cin >> t;
+    while(t--){
+        unsigned n;
+        std::cin>>n;
+        std::cout << n << '\n';
+    }
+    return 0;
+}
