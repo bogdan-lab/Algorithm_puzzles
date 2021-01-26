c2a461faa75177e5d29750cf174713c5522b93cf
bogdan <lakatosh@phystech.edu>
2021-01-26 21:35:10 +0300

    done

---
+++ Simple_stuff/task_1113A.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+
+
+
+int main(){
+    unsigned short n, v;
+    std::cin >> n >> v;
+    unsigned short pass_num = n-1;
+    if (pass_num<v){
+        std::cout << pass_num << '\n';
+        return 0;
+    }
+    unsigned remain = pass_num - v;
+    unsigned sum = v;
+    unsigned delta = 2;
+    while(remain--){
+        sum += delta;
+        delta++;
+    }
+    std::cout << sum << '\n';
+    return 0;
+}
