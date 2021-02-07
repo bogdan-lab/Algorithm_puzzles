8b59a98af6989c138fb22fd2a80d4b9e52f141ee
bogdan <lakatosh@phystech.edu>
2021-02-08 01:07:01 +0300

    prepared Byte vector. only writing to the file is left

--- Huffman_algo/encoder.cpp
+++ Huffman_algo/encoder.cpp
@@ -8,18 +8,18 @@
 #include<queue>
 #include<utility>
 #include<sstream>
+#include<bitset>
 
 using FreqHolder = std::unordered_map<char, size_t>;
-using BinWord = std::vector<short>;
-using Translator = std::unordered_map<char, BinWord>;
-
+using Translator = std::unordered_map<char, std::string>;
+using Byte = std::bitset<8>;
 
 struct HNode{
     HNode* left_ = nullptr;
     HNode* right_ = nullptr;
     size_t freq_ = 0;
     char symbol_ = '\0';
-    BinWord code_ = {};
+    std::string code_ = {};
 
     HNode(const size_t gf, const char gc) : freq_(gf), symbol_(gc) {}
     HNode(HNode* lhs, HNode* rhs) : left_(lhs), right_(rhs) {
@@ -46,23 +46,38 @@ void add_binary_words(HNode* symbol_tree);
 Translator get_translator_to_bin(const HNode* symbol_tree);
 void fill_tree_vector(const HNode* symbol_tree, std::vector<const HNode*>& tree_elements);
 
+std::string get_bin_repr_text(const std::string& content, const Translator& char_to_bin);
+std::vector<Byte> translate_content(const std::string& content, const Translator& char_to_bin);
+
 //int main(int argc, char** argv){
 int main(){
+
+    //TODO Entire text is one symbol - does not work!
+
     //std::cout <<argc << '\n';
     //std::cout << "THE FIRST ARGUMENT " << argv[1] << '\n';
     //std::cout << "THE SECOND ARGUMENT " << argv[2] << '\n' << '\n';
 
     //std::ifstream in_file(argv[1], std::ios_base::in);
     std::stringstream in_file;
-    in_file << R"(asdnad ads pamda asd)";
+    in_file << R"(Hello World!)";
     std::string content;
     std::copy(std::istreambuf_iterator<char>(in_file),
               std::istreambuf_iterator<char>(), std::back_inserter(content));
     FreqHolder frequencies = get_frequency(content);
-    //PriorQueue symbol_queue = build_queue(frequencies); 	//TODO DELETE
     HNode* symbol_tree  = build_HTree(frequencies);
     add_binary_words(symbol_tree);
     Translator char_to_bin = get_translator_to_bin(symbol_tree);
+    std::vector<Byte> bin_content = translate_content(content, char_to_bin);
+    //size_t symbol_num = content.size();
+
+
+    /*
+    Content of the encoded file:
+    1) translation information
+    2) number of symbols need this in order to stop deciphering last byte
+    3) ciphered text
+    */
 
 
     for(const auto el : char_to_bin){
@@ -73,6 +88,10 @@ int main(){
         std::cout << '\n';
     }
 
+    for(const auto el : bin_content){
+        std::cout << el << ' ';
+    }
+    std::cout << '\n';
 
     /*
     while(!symbol_queue.empty()){
@@ -88,6 +107,28 @@ int main(){
 
     return 0;
 }
+std::vector<Byte> translate_content(const std::string& content, const Translator& char_to_bin){
+    std::string bin_text = get_bin_repr_text(content, char_to_bin);
+    std::vector<Byte> bin_content;
+    size_t size = bin_text.size()/8;
+    size_t leftover = bin_text.size() % 8;
+    bin_content.reserve(size+1);
+    for(size_t i=0; i<size; i++){
+        bin_content.push_back({bin_text, 8*i, 8});
+    }
+    bin_content.push_back({bin_text, 8*size, leftover});
+    bin_content.back() <<= 8 - leftover;
+    return bin_content;
+}
+
+std::string get_bin_repr_text(const std::string& content, const Translator& char_to_bin){
+    std::string bin_text;
+    for(const auto& el : content){
+        bin_text.insert(bin_text.end(), char_to_bin.at(el).begin(), char_to_bin.at(el).end());
+    }
+    return bin_text;
+}
+
 
 
 Translator get_translator_to_bin(const HNode* symbol_tree){
@@ -112,10 +153,12 @@ void fill_tree_vector(const HNode* symbol_tree, std::vector<const HNode*>& tree_
 
 void add_binary_words(HNode* symbol_tree){
     if(!symbol_tree->left_ || !symbol_tree->right_) return;
+    symbol_tree->left_->code_.reserve(symbol_tree->code_.size()+1);
     symbol_tree->left_->code_ = symbol_tree->code_;
-    symbol_tree->left_->code_.push_back(0);
+    symbol_tree->left_->code_.push_back('0');
+    symbol_tree->right_->code_.reserve(symbol_tree->code_.size()+1);
     symbol_tree->right_->code_ = symbol_tree->code_;
-    symbol_tree->right_->code_.push_back(1);
+    symbol_tree->right_->code_.push_back('1');
     add_binary_words(symbol_tree->left_);
     add_binary_words(symbol_tree->right_);
 }
