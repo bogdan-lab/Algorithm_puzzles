8f42f02bb6e20af270d95b4fc3e9468f96f8c34a
bogdan <lakatosh@phystech.edu>
2021-05-08 15:02:50 +0300

    forgotten addition

--- Simple_stuff/task_520B.cpp
+++ Simple_stuff/task_520B.cpp
@@ -2,14 +2,13 @@
 
 int calc_btn_cnt(int n, int m, int btn_cnt) {
   if (n >= m) {
-    return btn_cnt + n-m;
+    return btn_cnt + n - m;
   }
-  //here n<m
-  if(m%2){
-      return calc_btn_cnt(n, m+1, ++btn_cnt);
-  }
-  else {
-      return calc_btn_cnt(n, m/2, ++btn_cnt);
+  // here n<m
+  if (m % 2) {
+    return calc_btn_cnt(n, m + 1, ++btn_cnt);
+  } else {
+    return calc_btn_cnt(n, m / 2, ++btn_cnt);
   }
 }
 
