d7b738a95ece52191f596b5179ea210087401e64
bogdan-lab <lakatosh@phystech.edu>
2021-08-19 01:48:10 +0300

    done

--- Tasks/CF_task_1398C.cpp
+++ Tasks/CF_task_1398C.cpp
@@ -1,14 +1,18 @@
 #include <algorithm>
 #include <iostream>
 #include <sstream>
-#include <vector>
 #include <string>
+#include <vector>
+#include <unordered_map>
+
+
+using Table = std::vector<std::vector<int>>;
 
 void solution(std::istream& input = std::cin);
 void run_tests();
 int to_int(char ch);
 
-size_t count_good_intervals(const std::vector<int>& full);
+uint64_t count_good_intervals(const std::vector<int>& full);
 std::vector<int> get_integral(const std::vector<int>& full);
 
 int main() {
@@ -27,7 +31,7 @@ void solution(std::istream& input) {
     full_interval.reserve(n);
     std::string rec;
     input >> rec;
-    for(size_t i=0; i<rec.size(); ++i){
+    for (size_t i = 0; i < rec.size(); ++i) {
       full_interval.push_back(to_int(rec[i]));
     }
 
@@ -54,24 +58,27 @@ void run_tests() {
 int to_int(char ch) { return static_cast<int>(ch) - 48; }
 
 std::vector<int> get_integral(const std::vector<int>& full) {
-  std::vector<int> res(full.size() + 1);
-  for (size_t i = 1; i < res.size(); ++i) {
-    res[i] = res[i - 1] + full[i - 1];
+  std::vector<int> res(full.size())  ;
+  int sum = 0;
+  for (size_t i = 0; i < res.size(); ++i) {
+    sum += full[i];
+      res[i] = sum;
   }
   return res;
 }
 
-size_t count_good_intervals(const std::vector<int>& full) {
-  std::vector<int> integral = get_integral(full);
-  size_t count = 0;
 
-  for (size_t i = 1; i < integral.size(); ++i) {
-    for (size_t j = i; j < integral.size(); ++j) {
-      int sub_sum = integral[j] - integral[i] + full[i-1];
-      int sub_idx = j - i + 1;
-      count += sub_sum == sub_idx;
+uint64_t count_good_intervals(const std::vector<int>& full){
+    std::vector<int> integral = get_integral(full);
+    std::unordered_map<int, uint64_t> point_count;
+    for(int i=0; i<integral.size(); ++i){
+        point_count[integral[i]-i-1]++;
     }
-  }
-
-  return count;
+    uint64_t count = 0;
+    for(const auto& el : point_count){
+        count += ((el.second - 1)*el.second)/2;
+    }
+    count += point_count[0];
+    return count;
 }
+
