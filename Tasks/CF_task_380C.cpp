7028277e65c40f807751d2ac715445be324da432
bogdan-lab <lakatosh@phystech.edu>
2021-08-27 22:02:07 +0300

    still too slow

--- Tasks/CF_task_380C.cpp
+++ Tasks/CF_task_380C.cpp
@@ -1,84 +1,96 @@
-﻿#include <iostream>
-#include <sstream>
-#include <unordered_map>
-#include <vector>
-#include <algorithm>
-#include <string>
+﻿#include <algorithm>
+#include <iostream>
 #include <iterator>
+#include <map>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
 #include <utility>
+#include <vector>
 
-void solve(std::istream& input=std::cin);
-void process_requests(const std::string& brackets, const std::vector<size_t>& bnds);
-size_t get_max_length(const std::string& brackets, const size_t start,
-                      const size_t finish);
-
-
+void solve(std::istream& input = std::cin);
 void run_tests();
-int main(){
-    std::ios_base::sync_with_stdio (false);
-    std::cin.tie(nullptr);
-    solve(std::cin);
-    return 0;
+
+using BracketIntervals = std::map<size_t, size_t>;  // open -> close
+
+BracketIntervals get_intervals(const std::string& brackets);
+size_t get_length(const BracketIntervals& vec);
+
+int main() {
+  //run_tests();
+   solve(std::cin);
+  return 0;
 }
 
-
-
-
-void solve(std::istream &input){
-    std::string brackets;
-    input >> brackets;
-    size_t m;
-    input >> m;
-    std::vector<size_t> bnds;
-    bnds.reserve(2*m);
-    std::copy_n(std::istream_iterator<size_t>(input), 2*m, std::back_inserter(bnds));
-    std::for_each(bnds.begin(), bnds.end(), [](size_t& el){el--;});
-    process_requests(brackets, bnds);
-}
-
-
-void process_requests(const std::string& brackets, const std::vector<size_t>& bnds){
-    for(size_t i=0; i<bnds.size()/2; i++){
-        std::cout << get_max_length(brackets, bnds[2*i], bnds[2*i+1]) << '\n';
+BracketIntervals get_intervals(const std::string& brackets) {
+  BracketIntervals res;
+  std::stack<size_t> open;
+  for (size_t i = 0; i < brackets.size(); ++i) {
+    switch (brackets[i]) {
+      case '(': {
+        open.push(i);
+        break;
+      }
+      case ')': {
+        if (!open.empty()) {
+          res[open.top()] = i;
+          open.pop();
+        }
+        break;
+      }
     }
+  }
+  return res;
 }
 
-size_t get_max_length(const std::string& brackets, const size_t start,
-                      const size_t finish){
-    if(finish<=start) return 0;
-    size_t begin = brackets.find('(', start);
-    if(begin==std::string::npos) return 0;
-    size_t end = brackets.rfind(')', finish);
-    if(end==std::string::npos || end<=begin) return 0;
-    size_t count_right = 0;
-    size_t count_left = 0;
-    for(size_t i=begin; i<end+1; i++){
-        count_right += brackets[i]=='(';
-        count_left += brackets[i]==')' && count_right>count_left;
-    }
-    return 2*count_left;
+size_t get_length(const BracketIntervals& scheme, size_t start, size_t end) {
+  auto start_it = scheme.lower_bound(start);
+  auto end_it = scheme.upper_bound(end);
+  size_t res = 0;
+  for (auto it = start_it; it != end_it; ++it) {
+    if (it->second <= end) res += 2;
+  }
+  return res;
 }
 
-
-void run_tests(){
-    {
-        std::stringstream ss;
-        ss << "())(())(())(\n";
-        ss << R"(7
-              1 1
-              2 3
-              1 2
-              1 12
-              8 12
-              5 11
-              2 10)";
-        solve(ss);
-    }
-    {
-        std::stringstream ss;
-        ss << "())(()\n";
-        ss<<R"(1
-            1 6)";
-        solve(ss);
-    }
+void solve(std::istream& input) {
+  std::string brackets;
+  input >> brackets;
+  BracketIntervals scheme = get_intervals(brackets);
+  size_t m;
+  input >> m;
+  while (m--) {
+    size_t s, e;
+    input >> s >> e;
+    std::cout << get_length(scheme, s - 1, e - 1) << '\n';
+  }
 }
+
+void run_tests() {
+  {
+    std::stringstream ss;
+    ss << R"(())(())(())(
+7
+1 1
+2 3
+1 2
+1 12
+8 12
+5 11
+2 10
+)";
+    solve(ss);
+    std::cout << "expected = 0 0 2 10 4 6 6\n";
+  }
+  {
+    std::stringstream ss;
+    ss << R"((
+1
+1 1
+)";
+    solve(ss);
+    std::cout << "expected = 0\n";
+  }
+}
+
