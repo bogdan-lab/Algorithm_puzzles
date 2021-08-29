41cf0bd247032a71dbbc1398b2c564c50a492366
bogdan-lab <lakatosh@phystech.edu>
2021-08-29 16:15:42 +0300

    build tree from leaves

--- Tasks/CF_task_380C.cpp
+++ Tasks/CF_task_380C.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <iterator>
 #include <map>
+#include <queue>
 #include <set>
 #include <sstream>
 #include <stack>
@@ -9,7 +10,6 @@
 #include <unordered_map>
 #include <utility>
 #include <vector>
-#include <queue>
 
 void solve(std::istream& input = std::cin);
 void run_tests();
@@ -18,101 +18,101 @@ struct Interval {
   size_t open = 0;
   size_t close = 0;
   size_t len = 0;
+  size_t unused_open = 0;
+  size_t unused_close = 0;
 };
 
 bool operator<(const Interval& lhs, const Interval& rhs) {
   return lhs.open < rhs.open || (lhs.open == rhs.open && lhs.close > rhs.close);
 }
 
-struct QueuComp{
-    bool operator()(const Interval& lhs, const Interval& rhs) const {
-        return lhs.len > rhs.len || (lhs.len== rhs.len && lhs.open > rhs.open);
-    }
-};
-
-using IntervalPull = std::priority_queue<Interval, std::vector<Interval>, QueuComp>;
-
-
 using BracketIntervals = std::set<Interval>;
 
-IntervalPull get_interval_pull(const std::string& brackets);
-
-
 size_t get_length(const BracketIntervals& vec);
-BracketIntervals build_tree(IntervalPull pull);
 
+BracketIntervals build_tree(const std::string& brackets);
+
+Interval connect_intervals(const Interval& lhs, const Interval& rhs);
 
 int main() {
-   run_tests();
-  //solve(std::cin);
+   //run_tests();
+  solve(std::cin);
   return 0;
 }
+Interval connect_intervals(const Interval& lhs, const Interval& rhs) {
+  Interval res;
+  res.open = lhs.open;
+  res.close = rhs.close;
+  size_t addition = std::min(lhs.unused_open, rhs.unused_close);
+  res.len = lhs.len + rhs.len + 2 * addition;
+  res.unused_open = lhs.unused_open + rhs.unused_open - addition;
+  res.unused_close = lhs.unused_close + rhs.unused_close - addition;
+  return res;
+}
 
-IntervalPull get_interval_pull(const std::string& brackets){
-  IntervalPull res;
-  std::stack<size_t> open;
+BracketIntervals build_tree(const std::string& brackets) {
+  BracketIntervals res;
+  std::queue<Interval> buff;
   for (size_t i = 0; i < brackets.size(); ++i) {
-    switch (brackets[i]) {
-      case '(': {
-        open.push(i);
-        break;
-      }
-      case ')': {
-        if (!open.empty()) {
-          res.push({open.top(), i, i - open.top() + 1});
-          open.pop();
-        }
-        break;
-      }
-    }
+    Interval tmp;
+    tmp.open = i;
+    tmp.close = i + 1;
+    tmp.unused_open = brackets[i] == '(';
+    tmp.unused_close = brackets[i] == ')';
+    buff.push(tmp);
   }
+  std::queue<Interval> next_buff;
+  while (true) {
+      if(buff.empty()){
+          buff.swap(next_buff);
+      }
+      if(buff.size()==1){
+          buff.swap(next_buff);
+          buff.push(next_buff.front());
+          next_buff.pop();
+      }
+      if(buff.size()<=1) break;
+
+    Interval lhs = buff.front();
+    buff.pop();
+    Interval rhs = buff.front();
+    buff.pop();
+    res.insert(lhs);
+    res.insert(rhs);
+    next_buff.push(connect_intervals(lhs, rhs));
+  }
+  res.insert(buff.front());
   return res;
 }
 
-BracketIntervals build_tree(IntervalPull pull){
-    BracketIntervals res;
-    //TODO clear inner -> merge by start position using buffer and swap -> return tree
-    while(pull.size() > 1){
-        Interval lhs = pull.top();
-        res.insert(lhs);
-        pull.pop();
-        Interval rhs = pull.top();
-        res.insert(rhs);
-        pull.pop();
-        pull.push({lhs.open, rhs.close, lhs.len+rhs.len});
-    }
-    res.insert(pull.top());
-    return res;
-}
-
-
-
-
-size_t get_length(const BracketIntervals& scheme, size_t start, size_t end) {
-  Interval fnd{start, end, 0};
-  size_t res = 0;
-  auto it = scheme.lower_bound(fnd);
-  while (it != scheme.end() && it->open <= end) {
-    if (it->close <= end) {
-      res += it->len;
-      it = scheme.upper_bound({it->close, end, 0});
-    } else {
-      ++it;
-    }
+size_t get_length(const std::vector<Interval>& scheme, size_t start,
+                  size_t end) {
+  if (end - start <= 1) return 0;
+  Interval fnd;
+  fnd.open = start;
+  fnd.close = end;
+  Interval res;
+  res.open = start;
+  auto it = std::lower_bound(scheme.begin(), scheme.end(), fnd);
+  while (it != scheme.end() && it->open < end) {
+    res = connect_intervals(res, *it);
+    fnd.open = it->close;
+    it = std::lower_bound(scheme.begin(), scheme.end(), fnd);
   }
-  return res;
+  return res.len;
 }
 
 void solve(std::istream& input) {
   std::string brackets;
   input >> brackets;
-  BracketIntervals scheme = build_tree(get_interval_pull(brackets));
+  BracketIntervals scheme = build_tree(brackets);
+  std::vector<Interval> vec(scheme.begin(), scheme.end());
   size_t m;
   input >> m;
   while (m--) {
     size_t s, e;
     input >> s >> e;
-    std::cout << get_length(scheme, s - 1, e - 1) << '\n';
+    std::cout << get_length(vec, s - 1, e) << '\n';
   }
 }
 
