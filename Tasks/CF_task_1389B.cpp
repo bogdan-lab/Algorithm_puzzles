1f0da0eb922dd131498c98f22805093cb24c94df
bogdan-lab <lakatosh@phystech.edu>
2021-12-31 00:03:55 +0300

    initial dynamic realization

--- Tasks/CF_task_1389B.cpp
+++ Tasks/CF_task_1389B.cpp
@@ -1,15 +1,17 @@
+#include <algorithm>
 #include <iostream>
+#include <limits>
+#include <numeric>
 #include <sstream>
 #include <vector>
 
-struct Step {
-  int prev_index;
-  int curr_index;
-};
+constexpr int kEmptyValue = std::numeric_limits<int>::min();
+using Data = std::vector<std::vector<std::vector<int>>>;
 
 void Solution(std::istream& input = std::cin);
 void RunTests();
-int CalcMaxScore(const std::vector<int>& data, int max_steps, int max_left);
+int CalcMaxScore(int curr_pos, int max_steps, int max_left, bool can_step_left,
+                 Data& data, const std::vector<int>& array);
 
 int main() {
   std::ios_base::sync_with_stdio(false);
@@ -27,31 +29,50 @@ void Solution(std::istream& input) {
     int max_steps;
     int max_left;
     input >> n >> max_steps >> max_left;
-    std::vector<int> data(n);
-    for (auto& el : data) {
+    std::vector<int> array(n);
+    for (auto& el : array) {
       input >> el;
     }
-    std::cout << CalcMaxScore(data, max_steps, max_left) << '\n';
+    Data data(array.size());
+    for (auto& el : data) {
+      el.resize(max_steps + 1);
+      for (auto& inner_el : el) {
+        inner_el.resize(max_left + 1, kEmptyValue);
+      }
+    }
+    std::cout << CalcMaxScore(0, max_steps, max_left, true, data, array)
+              << '\n';
   }
 }
 
-int CalcMaxScore(const std::vector<int>& data, int max_steps, int max_left) {
-  int count = data[0];
-  int prev_pos = 0;
-  int curr_pos = 0;
-  while (max_steps--) {
-    if (prev_pos < curr_pos && curr_pos > 0 && max_left > 0 &&
-        data[curr_pos - 1] > data[curr_pos + 1]) {
-      max_left--;
-      prev_pos = curr_pos;
-      curr_pos -= 1;
-    } else {
-      prev_pos = curr_pos;
-      curr_pos += 1;
-    }
-    count += data[curr_pos];
+int CalcMaxScore(int curr_pos, int max_steps, int max_left, bool can_step_left,
+                 Data& data, const std::vector<int>& array) {
+  if (data[curr_pos][max_steps][max_left] != kEmptyValue) {
+    return data[curr_pos][max_steps][max_left];
   }
-  return count;
+  if (max_steps == 0) {
+    data[curr_pos][max_steps][max_left] = array[curr_pos];
+    return data[curr_pos][max_steps][max_left];
+  }
+  if (max_left == 0) {
+    auto it = array.begin() + curr_pos;
+    data[curr_pos][max_steps][max_left] = std::accumulate(
+        it, it + std::min<int>(max_steps, array.size() - curr_pos), 0);
+    return data[curr_pos][max_steps][max_left];
+  }
+  int left_step = kEmptyValue;
+  if (curr_pos > 0 && max_left > 0 && can_step_left) {
+    left_step = CalcMaxScore(curr_pos - 1, max_steps - 1, max_left - 1, false,
+                             data, array);
+  }
+  int right_step = kEmptyValue;
+  if (curr_pos < array.size() - 1 && max_steps > 0) {
+    right_step =
+        CalcMaxScore(curr_pos + 1, max_steps - 1, max_left, true, data, array);
+  }
+  data[curr_pos][max_steps][max_left] =
+      array[curr_pos] + std::max(left_step, right_step);
+  return data[curr_pos][max_steps][max_left];
 }
 
 void RunTests() {
