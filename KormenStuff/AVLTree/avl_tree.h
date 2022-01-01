fa9e7df5a1e5beaa871da327491f792bb818b072
bogdan-lab <lakatosh@phystech.edu>
2022-01-01 23:00:38 +0300

    insert and exists

---
+++ KormenStuff/AVLTree/avl_tree.h
@@ -0,0 +1,82 @@
+﻿#ifndef AVL_TREE_H
+#define AVL_TREE_H
+
+template <typename Key, typename Value>
+class AVLTree {
+  struct Node {
+    Node(const Key& g_key, const Value& g_val) : key(g_key), value(g_val) {}
+
+    Key key;
+    Value value;
+    Node* left = nullptr;
+    Node* right = nullptr;
+    Node* parent = nullptr;
+  };
+
+ public:
+  AVLTree() = default;
+
+  ~AVLTree() {
+    DeleteTree(head_);
+    head_ = nullptr;
+  }
+
+  bool Insert(const Key& key, const Value& value) {
+    if (Empty()) {
+      head_ = new Node{key, value};
+      return true;
+    }
+    Node* current = head_;
+    Node* parent = nullptr;
+    while (current) {
+      parent = current->parent;
+      if (current->key < key) {
+        current = current->right;
+      } else if (key < current->key) {
+        current = current->left;
+      } else {
+        return false;
+      }
+    }
+    if (parent->key < key) {
+      parent->right = new Node{key, value};
+    } else {
+      parent->left = new Node{key, value};
+    }
+    return false;
+  }
+
+  bool Delete(const Key& key) { return false; }
+
+  bool Exists(const Key& key) const {
+    Node* current = head_;
+    while (current) {
+      if (current->key < key) {
+        current = current->right;
+      } else if (key < current->key) {
+        current = current->left;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  bool Empty() const { return !head_; }
+
+  const Node* Next() const { return nullptr; }
+
+  const Node* Prev() const { return nullptr; }
+
+ private:
+  static void DeleteTree(Node* tree_head) {
+    if (!tree_head) return;
+    DeleteTree(tree_head->left);
+    DeleteTree(tree_head->right);
+    delete tree_head;
+  }
+
+  Node* head_ = nullptr;
+};
+
+#endif
---
+++ KormenStuff/AVLTree/main.cpp
@@ -0,0 +1,14 @@
+﻿#include "avl_tree.h"
+#include "gtest/gtest.h"
+
+TEST(AVLTree, EmptyTree) {
+  AVLTree<int, int> test;
+  EXPECT_TRUE(test.Empty());
+  EXPECT_FALSE(test.Exists(1));
+  EXPECT_FALSE(test.Exists(0));
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
