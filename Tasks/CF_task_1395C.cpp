bda8011e30d1a606c3a147e03bcd5e36080a8d6b
bogdan-lab <lakatosh@phystech.edu>
2022-01-05 18:25:46 +0300

    WA 9

--- Tasks/CF_task_1395C.cpp
+++ Tasks/CF_task_1395C.cpp
@@ -6,15 +6,15 @@
 
 void Solution(std::istream& input = std::cin);
 void RunTests();
-int GetMinCollission(int val, const std::vector<int>& vec);
-int ComposeMinVals(int pos_idx, const std::vector<int>& pick_bin,
-                   const std::vector<int>& match_bin);
+int UpdateMinVal(int curr_min_val, int val, const std::vector<int>& match_bin);
+std::vector<int> BuildMinVals(const std::vector<int>& pick_bin,
+                              const std::vector<int>& match_bin);
 
 int main() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
-  // RunTests();
-  Solution(std::cin);
+  RunTests();
+  // Solution(std::cin);
   return 0;
 }
 
@@ -22,73 +22,88 @@ void Solution(std::istream& input) {
   int n = 0;
   int m = 0;
   input >> n >> m;
-  std::vector<int> lhs(n);
-  for (auto& el : lhs) {
+  std::vector<int> pick_bin(n);
+  for (auto& el : pick_bin) {
     input >> el;
   }
-  std::vector<int> rhs(m);
-  for (auto& el : rhs) {
+  std::vector<int> match_bin(m);
+  for (auto& el : match_bin) {
     input >> el;
   }
-  std::vector<int> min_vals;
-  min_vals.reserve(lhs.size());
-  for (int i = 0; i < lhs.size(); ++i) {
-    min_vals.push_back(ComposeMinVals(i, lhs, rhs));
-  }
-  int min_val = std::numeric_limits<int>::max();
-  for (const auto& el : min_vals) {
-    min_val = std::min(min_val, el);
-  }
-  std::cout << min_val << '\n';
+
+  std::vector<int> min_vals = BuildMinVals(pick_bin, match_bin);
+  std::cout << *std::min_element(min_vals.begin(), min_vals.end()) << '\n';
 }
 
-int GetMinCollission(int val, const std::vector<int>& vec) {
-  int min_val = val & vec.front();
-  for (const auto& el : vec) {
-    min_val = std::min(min_val, val & el);
+void Print(const std::vector<int>& data) {
+  for (const auto& el : data) {
+    std::cout << el << ' ';
   }
-  return min_val;
+  std::cout << '\n';
 }
 
-int ComposeMinVals(int pos_idx, const std::vector<int>& pick_bin,
-                   const std::vector<int>& match_bin) {
-  int res = GetMinCollission(pick_bin[pos_idx], match_bin);
+std::vector<int> BuildMinVals(const std::vector<int>& pick_bin,
+                              const std::vector<int>& match_bin) {
+  std::vector<int> result;
+  result.reserve(match_bin.size());
+  for (const auto& el : match_bin) {
+    result.push_back((pick_bin.front() & el));
+  }
+  Print(result);
   for (const auto& el : pick_bin) {
-    int min_change = std::numeric_limits<int>::max();
-    for (const auto& collider : match_bin) {
-      min_change = std::min(min_change, (res | (el & collider)) - res);
+    for (auto& res_el : result) {
+      res_el = UpdateMinVal(res_el, el, match_bin);
     }
-    res += min_change;
+    Print(result);
   }
-  return res;
+  return result;
+}
+
+int UpdateMinVal(int curr_min_val, int val, const std::vector<int>& match_bin) {
+  int min_change = std::numeric_limits<int>::max();
+  for (const auto& el : match_bin) {
+    min_change =
+        std::min(min_change, (curr_min_val | (el & val)) - curr_min_val);
+  }
+  return curr_min_val + min_change;
 }
 
 void RunTests() {
   {
     std::stringstream ss;
-    ss << R"(4 2
-2 6 4 0
-2 4
+    ss << R"(19 16
+272 140 389 278 27 131 328 422 153 414 222 507 405 445 200 243 403 82 317
+447 73 189 396 72 494 492 386 203 226 389 482 311 500 344 360
 )";
     Solution(ss);
-    std::cout << "expected = 2\n";
+    std::cout << "expected = 72\n";
   }
-  {
-    std::stringstream ss;
-    ss << R"(7 6
-1 9 1 9 8 1 0
-1 1 4 5 1 4
-)";
-    Solution(ss);
-    std::cout << "expected = 0\n";
-  }
-  {
-    std::stringstream ss;
-    ss << R"(8 5
-179 261 432 162 82 43 10 38
-379 357 202 184 197
-)";
-    Solution(ss);
-    std::cout << "expected = 147\n";
-  }
+  /*
+    {
+      std::stringstream ss;
+      ss << R"(4 2
+  2 6 4 0
+  2 4
+  )";
+      Solution(ss);
+      std::cout << "expected = 2\n";
+    }
+    {
+      std::stringstream ss;
+      ss << R"(7 6
+  1 9 1 9 8 1 0
+  1 1 4 5 1 4
+  )";
+      Solution(ss);
+      std::cout << "expected = 0\n";
+    }
+    {
+      std::stringstream ss;
+      ss << R"(8 5
+  179 261 432 162 82 43 10 38
+  379 357 202 184 197
+  )";
+      Solution(ss);
+      std::cout << "expected = 147\n";
+    }*/
 }
