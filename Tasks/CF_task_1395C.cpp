c86e7707dc64b60f80533d4c5cf45ecc331472ff
bogdan-lab <lakatosh@phystech.edu>
2022-01-06 17:49:04 +0300

    done

--- Tasks/CF_task_1395C.cpp
+++ Tasks/CF_task_1395C.cpp
@@ -1,14 +1,18 @@
 #include <algorithm>
+#include <cassert>
 #include <iostream>
 #include <limits>
+#include <numeric>
 #include <sstream>
 #include <vector>
 
 void Solution(std::istream& input = std::cin);
 void RunTests();
-int UpdateMinVal(int curr_min_val, int val, const std::vector<int>& match_bin);
-int GetBestFor(int index, const std::vector<int>& pick_bin,
-               const std::vector<int>& match_bin);
+int GetMinPossibleAnswer(const std::vector<int>& all_answers,
+                         const std::vector<int>& pick_bin,
+                         const std::vector<int>& match_bin);
+bool CheckValidAnswer(int ans, const std::vector<int>& pick_bin,
+                      const std::vector<int>& match_bin);
 
 int main() {
   std::ios_base::sync_with_stdio(false);
@@ -30,45 +34,38 @@ void Solution(std::istream& input) {
   for (auto& el : match_bin) {
     input >> el;
   }
-
-  std::vector<int> min_vals;
-  min_vals.reserve(pick_bin.size());
-  for (int i = 0; i < pick_bin.size(); ++i) {
-    min_vals.push_back(GetBestFor(i, pick_bin, match_bin));
-  }
-  std::cout << *std::min_element(min_vals.begin(), min_vals.end()) << '\n';
+  std::vector<int> all_answers(512);
+  std::iota(all_answers.begin(), all_answers.end(), 0);
+  std::cout << GetMinPossibleAnswer(all_answers, pick_bin, match_bin) << '\n';
 }
 
-void Print(const std::vector<int>& data) {
-  for (const auto& el : data) {
-    std::cout << el << ' ';
-  }
-  std::cout << '\n';
-}
-
-int GetBestFor(int index, const std::vector<int>& pick_bin,
-               const std::vector<int>& match_bin) {
-  std::vector<int> result;
-  result.reserve(match_bin.size());
-  for (const auto& el : match_bin) {
-    result.push_back((pick_bin[index] & el));
-  }
-  for (const auto& el : pick_bin) {
-    for (auto& res_el : result) {
-      res_el = UpdateMinVal(res_el, el, match_bin);
+int GetMinPossibleAnswer(const std::vector<int>& all_answers,
+                         const std::vector<int>& pick_bin,
+                         const std::vector<int>& match_bin) {
+  for (const auto& el : all_answers) {
+    if (CheckValidAnswer(el, pick_bin, match_bin)) {
+      return el;
     }
   }
-  return *std::min_element(result.begin(), result.end());
+  assert(false);
 }
 
-int UpdateMinVal(int curr_min_val, int val, const std::vector<int>& match_bin) {
-  int min_change = std::numeric_limits<int>::max();
-  for (const auto& el : match_bin) {
-    min_change =
-        std::min(min_change, (curr_min_val | (el & val)) - curr_min_val);
+bool CheckValidAnswer(int ans, const std::vector<int>& pick_bin,
+                      const std::vector<int>& match_bin) {
+  for (const auto& p : pick_bin) {
+    bool found_pair = false;
+    for (const auto& m : match_bin) {
+      if ((ans | (m & p)) == ans) {
+        found_pair = true;
+        break;
+      }
+    }
+    if (!found_pair) {
+      return false;
+    }
   }
-  return curr_min_val + min_change;
-}
+  return true;
+};
 
 void RunTests() {
   {
@@ -83,27 +80,27 @@ void RunTests() {
   {
     std::stringstream ss;
     ss << R"(4 2
-  2 6 4 0
-  2 4
-  )";
+2 6 4 0
+2 4
+)";
     Solution(ss);
     std::cout << "expected = 2\n";
   }
   {
     std::stringstream ss;
     ss << R"(7 6
-  1 9 1 9 8 1 0
-  1 1 4 5 1 4
-  )";
+1 9 1 9 8 1 0
+1 1 4 5 1 4
+)";
     Solution(ss);
     std::cout << "expected = 0\n";
   }
   {
     std::stringstream ss;
     ss << R"(8 5
-  179 261 432 162 82 43 10 38
-  379 357 202 184 197
-  )";
+179 261 432 162 82 43 10 38
+379 357 202 184 197
+)";
     Solution(ss);
     std::cout << "expected = 147\n";
   }
