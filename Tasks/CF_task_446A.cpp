fe619208cf6ce55e6b73bbebd78f62dd746aa4b5
bogdan-lab <lakatosh@phystech.edu>
2022-01-07 16:26:23 +0300

    done

--- Tasks/CF_task_446A.cpp
+++ Tasks/CF_task_446A.cpp
@@ -1,25 +1,20 @@
+#include <algorithm>
 #include <iostream>
 #include <limits>
 #include <sstream>
 #include <vector>
 
-struct Range {
-  int last_val = 0;
-  int first_val = 0;
-  int length = 0;
-};
-
 void Solution(std::istream& input = std::cin);
 void RunTests();
 int GetMaxLength(const std::vector<int>& data);
-int GetMaxLength2(const std::vector<int>& data);
-std::vector<Range> GetMaxNoChangeLength(const std::vector<int>& data);
+std::vector<int> GetSuffixLength(const std::vector<int>& data);
+std::vector<int> GetPrefixLength(const std::vector<int>& data);
 
 int main() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
-  RunTests();
-  // Solution(std::cin);
+  // RunTests();
+  Solution(std::cin);
   return 0;
 }
 
@@ -30,71 +25,53 @@ void Solution(std::istream& input) {
   for (auto& el : data) {
     input >> el;
   }
-  std::cout << GetMaxLength2(data) << '\n';
+  std::cout << GetMaxLength(data) << '\n';
 }
 
-std::vector<Range> GetMaxNoChangeLength(const std::vector<int>& data) {
-  std::vector<Range> result;
-  result.reserve(data.size());
-  Range rng{.last_val = data.front(), .first_val = data.front(), .length = 1};
-  for (int i = 1; i < data.size(); ++i) {
-    if (data[i] > data[i - 1]) {
-      rng.length += 1;
+std::vector<int> GetSuffixLength(const std::vector<int>& data) {
+  std::vector<int> result(data.size(), 0);
+  int curr_len = 1;
+  result.back() = curr_len;
+  for (int i = data.size() - 2; i >= 0; --i) {
+    if (data[i + 1] > data[i]) {
+      curr_len += 1;
     } else {
-      result.push_back(rng);
-      rng.length = 1;
-      rng.first_val = data[i];
+      curr_len = 1;
     }
-    rng.last_val = data[i];
+    result[i] = curr_len;
   }
-  result.push_back(rng);
   return result;
 }
 
-int GetMaxLength2(const std::vector<int>& data) {
-  std::vector<Range> no_change_length = GetMaxNoChangeLength(data);
-  int max_length = no_change_length.front().length;
-  for (int i = 1; i < no_change_length.size(); ++i) {
-    max_length = std::max(max_length, no_change_length[i].length +
-                                          no_change_length[i - 1].length);
+std::vector<int> GetPrefixLength(const std::vector<int>& data) {
+  std::vector<int> result(data.size(), 0);
+  int curr_len = 0;
+  for (int i = 0; i < result.size(); ++i) {
+    result[i] = curr_len;
+    if (i == 0 || data[i] > data[i - 1]) {
+      curr_len++;
+    } else {
+      curr_len = 1;
+    }
   }
-  return max_length;
+  return result;
 }
 
 int GetMaxLength(const std::vector<int>& data) {
-  int max_length = 1;
-  int curr_len = 1;
-  int prev_val = data.front();
-  int changed_pos = -1;
-  bool can_change = true;
-  for (int i = 1; i < data.size(); ++i) {
-    if (data[i] > prev_val) {
-      curr_len += 1;
-      prev_val = data[i];
-    } else {
-      if (can_change) {
-        can_change = false;
-        curr_len += 1;
-        prev_val += 1;
-        changed_pos = i;
-      } else {
-        int new_start_pos = data[changed_pos] < data[changed_pos + 1]
-                                ? changed_pos
-                                : changed_pos + 1;
-        curr_len = i - new_start_pos + 1;
-        if (data[i] > data[i - 1]) {
-          // no need to change now -> changing the first value
-          curr_len += 1;
-          prev_val = data[i];
-        } else {
-          prev_val += 1;
-          changed_pos = i;
-        }
-      }
+  std::vector<int> sufix_length = GetSuffixLength(data);
+  std::vector<int> prefix_length = GetPrefixLength(data);
+  std::vector<int> possible_ans;
+  possible_ans.reserve(data.size());
+  for (int i = 0; i < data.size() - 1; ++i) {
+    int pure = sufix_length[i];
+    int changed = std::max(prefix_length[i] + 1, 1 + sufix_length[i + 1]);
+    if (i == 0 || data[i + 1] - data[i - 1] >= 2) {
+      changed = prefix_length[i] + 1 + sufix_length[i + 1];
     }
-    max_length = std::max(max_length, curr_len);
+    possible_ans.push_back(std::max(pure, changed));
   }
-  return max_length;
+  possible_ans.push_back(prefix_length.back() + 1);
+  return *std::max_element(possible_ans.begin(), possible_ans.end());
 }
 
 void RunTests() {
@@ -126,6 +103,38 @@ void RunTests() {
     std::stringstream ss;
     ss << R"(6
 7 8 9 1 2 3
+)";
+    Solution(ss);
+    std::cout << "expected = 4\n";
+  }
+  {
+    std::stringstream ss;
+    ss << R"(5
+1 2 3 4 5
+)";
+    Solution(ss);
+    std::cout << "expected = 5\n";
+  }
+  {
+    std::stringstream ss;
+    ss << R"(4
+1 4 3 4
+)";
+    Solution(ss);
+    std::cout << "expected = 4\n";
+  }
+  {
+    std::stringstream ss;
+    ss << R"(4
+1 2 3 1
+)";
+    Solution(ss);
+    std::cout << "expected = 4\n";
+  }
+  {
+    std::stringstream ss;
+    ss << R"(6
+7 2 3 1 4 5
 )";
     Solution(ss);
     std::cout << "expected = 4\n";
