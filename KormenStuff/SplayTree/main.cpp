1d8e5aa4e746823a43394f24247039e4022e1a8c
bogdan-lab <lakatosh@phystech.edu>
2022-03-28 22:58:17 +0300

    Start building SplayTree on the AVL tree base

---
+++ KormenStuff/SplayTree/main.cpp
@@ -0,0 +1,318 @@
+﻿#include "gtest/gtest.h"
+#include "splay_tree.h"
+
+TEST(AVLTree, EmptyTree) {
+  AVLTree<int, int> test;
+  EXPECT_TRUE(test.Empty());
+  EXPECT_FALSE(test.Exists(1));
+  EXPECT_FALSE(test.Exists(0));
+}
+
+TEST(AVLTree, InsertExists) {
+  AVLTree<int, int> test;
+  EXPECT_TRUE(test.Empty());
+  test.Insert(5, 0);
+  EXPECT_FALSE(test.Empty());
+  test.Insert(3, 0);
+  test.Insert(4, 0);
+  test.Insert(2, 0);
+  test.Insert(7, 0);
+  test.Insert(6, 0);
+  test.Insert(8, 0);
+  EXPECT_TRUE(test.Exists(5));
+  EXPECT_TRUE(test.Exists(3));
+  EXPECT_TRUE(test.Exists(7));
+  EXPECT_TRUE(test.Exists(2));
+  EXPECT_TRUE(test.Exists(4));
+  EXPECT_TRUE(test.Exists(6));
+  EXPECT_TRUE(test.Exists(8));
+
+  test.Insert(5, 2);
+  test.Insert(3, 2);
+  test.Insert(4, 2);
+  test.Insert(2, 2);
+  test.Insert(6, 2);
+  test.Insert(7, 2);
+  test.Insert(8, 2);
+}
+
+TEST(AVLTree, NextPrevExisting) {
+  AVLTree<int, int> test;
+  test.Insert(5, 0);
+  test.Insert(3, 0);
+  test.Insert(4, 0);
+  test.Insert(2, 0);
+  test.Insert(7, 0);
+  test.Insert(6, 0);
+  test.Insert(8, 0);
+
+  EXPECT_EQ(test.Prev(5)->key, 4);
+  EXPECT_EQ(test.Next(5)->key, 6);
+  EXPECT_EQ(test.Prev(3)->key, 2);
+  EXPECT_EQ(test.Next(3)->key, 4);
+  EXPECT_EQ(test.Prev(7)->key, 6);
+  EXPECT_EQ(test.Next(7)->key, 8);
+  EXPECT_EQ(test.Prev(8)->key, 7);
+  EXPECT_FALSE(test.Next(8));
+  EXPECT_FALSE(test.Next(100));
+  EXPECT_FALSE(test.Prev(2));
+  EXPECT_FALSE(test.Prev(-50));
+  EXPECT_EQ(test.Next(2)->key, 3);
+}
+
+TEST(AVLTree, NextPrevNonexisting) {
+  AVLTree<int, int> test;
+  test.Insert(50, 0);
+  test.Insert(30, 0);
+  test.Insert(40, 0);
+  test.Insert(20, 0);
+  test.Insert(70, 0);
+  test.Insert(60, 0);
+  test.Insert(80, 0);
+
+  EXPECT_EQ(test.Prev(45)->key, 40);
+  EXPECT_EQ(test.Next(45)->key, 50);
+  EXPECT_EQ(test.Prev(55)->key, 50);
+  EXPECT_EQ(test.Next(55)->key, 60);
+  EXPECT_EQ(test.Prev(35)->key, 30);
+  EXPECT_EQ(test.Next(35)->key, 40);
+  EXPECT_EQ(test.Prev(65)->key, 60);
+  EXPECT_EQ(test.Next(65)->key, 70);
+  EXPECT_EQ(test.Prev(25)->key, 20);
+  EXPECT_EQ(test.Next(25)->key, 30);
+  EXPECT_EQ(test.Prev(75)->key, 70);
+  EXPECT_EQ(test.Next(75)->key, 80);
+}
+
+TEST(AVLTree, DeleteLeafs) {
+  AVLTree<int, int> test;
+  test.Insert(5, 0);
+  test.Insert(3, 0);
+  test.Insert(4, 0);
+  test.Insert(2, 0);
+  test.Insert(7, 0);
+  test.Insert(6, 0);
+  test.Insert(8, 0);
+
+  test.Delete(2);
+  EXPECT_FALSE(test.Exists(2));
+  test.Delete(4);
+  EXPECT_FALSE(test.Exists(4));
+  test.Delete(6);
+  EXPECT_FALSE(test.Exists(6));
+  test.Delete(8);
+  EXPECT_FALSE(test.Exists(8));
+  test.Delete(3);
+  EXPECT_FALSE(test.Exists(3));
+  test.Delete(7);
+  EXPECT_FALSE(test.Exists(7));
+  test.Delete(5);
+  EXPECT_FALSE(test.Exists(5));
+  EXPECT_TRUE(test.Empty());
+}
+
+TEST(AVLTree, DeleteInsideNodesWithLeafMax1) {
+  AVLTree<int, int> test;
+  test.Insert(50, 0);
+  test.Insert(30, 0);
+  test.Insert(40, 0);
+  test.Insert(20, 0);
+  test.Insert(70, 0);
+  test.Insert(60, 0);
+  test.Insert(80, 0);
+  test.Insert(10, 0);
+  test.Insert(25, 0);
+  test.Insert(35, 0);
+  test.Insert(45, 0);
+  test.Insert(55, 0);
+  test.Insert(65, 0);
+  test.Insert(75, 0);
+  test.Insert(85, 0);
+
+  test.Delete(30);
+  EXPECT_FALSE(test.Exists(30));
+  test.Delete(70);
+  EXPECT_FALSE(test.Exists(70));
+}
+
+TEST(AVLTree, DeleteInsideNodesWithLeafMin) {
+  AVLTree<int, int> test;
+  test.Insert(50, 0);
+  test.Insert(30, 0);
+  test.Insert(20, 0);
+  test.Insert(70, 0);
+  test.Insert(60, 0);
+  test.Insert(10, 0);
+  test.Insert(25, 0);
+  test.Insert(55, 0);
+  test.Insert(65, 0);
+
+  test.Delete(30);
+  EXPECT_FALSE(test.Exists(30));
+  test.Delete(70);
+  EXPECT_FALSE(test.Exists(70));
+}
+
+TEST(AVLTree, DeleteInsideNodesWithLeafMax2) {
+  AVLTree<int, int> test;
+  test.Insert(50, 0);
+  test.Insert(30, 0);
+  test.Insert(40, 0);
+  test.Insert(70, 0);
+  test.Insert(80, 0);
+  test.Insert(35, 0);
+  test.Insert(45, 0);
+  test.Insert(75, 0);
+  test.Insert(85, 0);
+
+  test.Delete(30);
+  EXPECT_FALSE(test.Exists(30));
+  test.Delete(70);
+  EXPECT_FALSE(test.Exists(70));
+}
+
+TEST(AVLTree, DeleteInsideNodeNotLeafMax) {
+  AVLTree<int, int> test;
+  test.Insert(50, 0);
+  test.Insert(30, 0);
+  test.Insert(40, 0);
+  test.Insert(20, 0);
+  test.Insert(70, 0);
+  test.Insert(60, 0);
+  test.Insert(80, 0);
+  test.Insert(10, 0);
+  test.Insert(25, 0);
+  test.Insert(35, 0);
+  test.Insert(45, 0);
+  test.Insert(55, 0);
+  test.Insert(65, 0);
+  test.Insert(75, 0);
+  test.Insert(85, 0);
+  test.Insert(37, 0);
+
+  test.Delete(30);
+  EXPECT_FALSE(test.Exists(30));
+}
+
+TEST(AVLTree, DeleteInsideNodeNotLeafMin) {
+  AVLTree<int, int> test;
+  test.Insert(50, 0);
+  test.Insert(30, 0);
+  test.Insert(20, 0);
+  test.Insert(70, 0);
+  test.Insert(60, 0);
+  test.Insert(80, 0);
+  test.Insert(10, 0);
+  test.Insert(25, 0);
+  test.Insert(55, 0);
+  test.Insert(65, 0);
+  test.Insert(75, 0);
+  test.Insert(85, 0);
+  test.Insert(23, 0);
+
+  test.Delete(30);
+  EXPECT_FALSE(test.Exists(30));
+}
+
+TEST(AVLTree, DeleteLastButOneMax) {
+  AVLTree<int, int> test;
+  test.Insert(50, 0);
+  test.Insert(30, 0);
+  test.Insert(40, 0);
+  test.Insert(20, 0);
+  test.Insert(70, 0);
+  test.Insert(60, 0);
+  test.Insert(80, 0);
+
+  test.Delete(30);
+  EXPECT_FALSE(test.Exists(30));
+  test.Delete(70);
+  EXPECT_FALSE(test.Exists(70));
+  EXPECT_FALSE(test.Exists(45));
+}
+
+TEST(AVLTree, DeleteLastButOneMin) {
+  AVLTree<int, int> test;
+  test.Insert(50, 0);
+  test.Insert(30, 0);
+  test.Insert(20, 0);
+  test.Insert(70, 0);
+  test.Insert(60, 0);
+
+  test.Delete(30);
+  EXPECT_FALSE(test.Exists(30));
+  test.Delete(70);
+  EXPECT_FALSE(test.Exists(70));
+}
+
+TEST(AVLTree, DeleteHeadBothBranchesFull) {
+  AVLTree<int, int> test;
+  test.Insert(5, 0);
+  test.Insert(3, 0);
+  test.Insert(4, 0);
+  test.Insert(2, 0);
+  test.Insert(7, 0);
+  test.Insert(6, 0);
+  test.Insert(8, 0);
+
+  test.Delete(5);
+  EXPECT_FALSE(test.Exists(5));
+  EXPECT_FALSE(test.Empty());
+  EXPECT_TRUE(test.Exists(3));
+  EXPECT_TRUE(test.Exists(4));
+  EXPECT_TRUE(test.Exists(2));
+  EXPECT_TRUE(test.Exists(6));
+  EXPECT_TRUE(test.Exists(7));
+  EXPECT_TRUE(test.Exists(8));
+}
+
+TEST(AVLTree, DeleteHeadOnlyLeftBranchPresent) {
+  AVLTree<int, int> test;
+  test.Insert(5, 0);
+  test.Insert(3, 0);
+  test.Insert(4, 0);
+  test.Insert(2, 0);
+
+  test.Delete(5);
+  EXPECT_FALSE(test.Exists(5));
+  EXPECT_FALSE(test.Empty());
+  EXPECT_TRUE(test.Exists(3));
+  EXPECT_TRUE(test.Exists(4));
+  EXPECT_TRUE(test.Exists(2));
+}
+
+TEST(AVLTree, DeleteHead) {
+  AVLTree<int, int> test;
+  test.Insert(5, 0);
+
+  test.Delete(5);
+  EXPECT_FALSE(test.Exists(5));
+  EXPECT_TRUE(test.Empty());
+}
+
+TEST(AVLTree, DeleteHeadOnlyOneOnTheLeft) {
+  AVLTree<int, int> test;
+  test.Insert(5, 0);
+  test.Insert(3, 0);
+
+  test.Delete(5);
+  EXPECT_FALSE(test.Exists(5));
+  EXPECT_FALSE(test.Empty());
+  EXPECT_TRUE(test.Exists(3));
+}
+
+TEST(AVLTree, DeleteHeadOnlyOneOnTheRight) {
+  AVLTree<int, int> test;
+  test.Insert(5, 0);
+  test.Insert(8, 0);
+
+  test.Delete(5);
+  EXPECT_FALSE(test.Exists(5));
+  EXPECT_FALSE(test.Empty());
+  EXPECT_TRUE(test.Exists(8));
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
---
+++ KormenStuff/SplayTree/splay_tree.h
@@ -0,0 +1,341 @@
+﻿#ifndef SPLAY_TREE_H
+#define SPLAY_TREE_H
+
+#include <algorithm>
+#include <cassert>
+#include <utility>
+
+template <typename Key, typename Value>
+class SplayTree {
+  struct Node {
+    Node(const Key& g_key, const Value& g_value) : key(g_key), value(g_value) {}
+
+    Node* left = nullptr;
+    Node* right = nullptr;
+    Node* parent = nullptr;
+    Key key;
+    Value value;
+  };
+
+ public:
+  SplayTree() = default;
+  ~SplayTree() {
+    DeleteImpl(root_);
+    root_ = nullptr;
+  }
+  void Insert(const Key& key, const Value& value) {
+    // Split and create
+  }
+  void Delete(const Key& key) {
+    // Split and merge
+  }
+  bool Empty() const { return !root_; }
+  Value* Find(const Key& key) { return FindImpl(root_, key); }
+
+ private:
+  static void DeleteImpl(Node* node) {
+    if (!node) return;
+    if (node->right) DeleteImpl(node->right);
+    if (node->left) DeleteImpl(node->left);
+    delete node;
+  }
+  /**Call when node is left child*/
+  static void RotateRight(Node* node) {
+    assert(node && node->parent);
+    Node* parent = node->parent;
+    // parent connection
+    Node* grand = parent->parent;
+    node->parent = grand;
+    if (grand) {
+      if (grand->left == parent) {
+        grand->left = node;
+      } else {
+        grand->right = node;
+      }
+    }
+    parent->parent = node;
+    // child connection
+    parent->left = node->right;
+    parent->left->parent = parent;
+    node->right = parent;
+  }
+  /**Call when node is right child*/
+  static void RotateLeft(Node* node) {
+    assert(node && node->parent);
+    Node* parent = node->parent;
+    // parent connection
+    Node* grand = parent->parent;
+    node->parent = grand;
+    if (grand) {
+      if (grand->left == parent) {
+        grand->left = node;
+      } else {
+        grand->right = node;
+      }
+    }
+    parent->parent = node;
+    // child connection
+    parent->right = node->left;
+    parent->right->parent = parent;
+    node->left = parent;
+  }
+
+  static void ZigZigRight(Node* node) {
+    RotateRight(node->parent);
+    RotateRight(node);
+  }
+
+  static void Splay(Node* node) {
+    Node* parent = node->parent;
+    if (!parent) return;
+    Node* grand = parent->parent;
+    if (!grand) {
+      if (node == parent->left) {
+        RotateRight(node);
+      } else {
+        RotateLeft(node);
+      }
+      return;
+    }
+    // Here we have grandparent
+    if (parent->left == node && grand->left == parent) {
+      RotateRight(node->parent);
+      RotateRight(node);
+      Splay(node);
+      return;
+    }
+    if (parent->right == node && grand->right == parent) {
+      RotateLeft(node->parent);
+      RotateLeft(node);
+      Splay(node);
+      return;
+    }
+    if (parent->right == node && grand->left == parent) {
+      RotateLeft(node);
+      RotateRight(node);
+      Splay(node);
+      return;
+    }
+    if (parent->left == node && grand->right == parent) {
+      RotateRight(node);
+      RotateLeft(node);
+      Splay(node);
+      return;
+    }
+  }
+
+  static Value* FindImpl(Node* node, const Key& key) {
+    if (!node) return node;
+    if (key < node->key) {
+      return Find(node->left, key);
+    } else if (node->key < key) {
+      return Find(node->right, key);
+    } else {
+      return &(node->value);
+    }
+    // TODO add node propagation!!! use Splay for it
+  }
+
+  Node* root_ = nullptr;
+};
+
+template <typename Key, typename Value>
+class AVLTree {
+  struct Node {
+    Node(const Key& g_key, const Value& g_val) : key(g_key), value(g_val) {}
+
+    Key key;
+    Value value;
+    int height = 1;
+    Node* left = nullptr;
+    Node* right = nullptr;
+  };
+
+ public:
+  AVLTree() = default;
+  AVLTree(const AVLTree&) = delete;
+  AVLTree(AVLTree&&) = delete;
+  AVLTree& operator=(const AVLTree&) = delete;
+  AVLTree& operator=(AVLTree&&) = delete;
+
+  ~AVLTree() {
+    DeleteTree(head_);
+    head_ = nullptr;
+  }
+
+  void Insert(const Key& key, const Value& val) {
+    head_ = InsertImpl(head_, key, val);
+  }
+
+  void Delete(const Key& key) { head_ = DeleteImpl(head_, key); }
+
+  bool Exists(const Key& key) const {
+    Node* current = head_;
+    while (current) {
+      if (current->key < key) {
+        current = current->right;
+      } else if (key < current->key) {
+        current = current->left;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  const Node* Next(const Key& key) const { return NextImpl(head_, key); }
+  const Node* Prev(const Key& key) const { return PrevImpl(head_, key); }
+
+  bool Empty() const { return !head_; }
+
+ private:
+  static void DeleteTree(Node* tree_head) {
+    if (!tree_head) return;
+    DeleteTree(tree_head->left);
+    DeleteTree(tree_head->right);
+    delete tree_head;
+  }
+
+  static int GetHeight(Node* node) { return node ? node->height : 0; }
+
+  static int GetFactor(Node* node) {
+    return GetHeight(node->right) - GetHeight(node->left);
+  }
+
+  static void FixHeight(Node* node) {
+    node->height = std::max(GetHeight(node->left), GetHeight(node->right)) + 1;
+  }
+
+  static Node* RotateRight(Node* head) {
+    assert(head);
+    Node* new_head = head->left;
+    head->left = new_head->right;
+    new_head->right = head;
+    FixHeight(head);
+    FixHeight(new_head);
+    return new_head;
+  }
+
+  static Node* RotateLeft(Node* head) {
+    assert(head);
+    Node* new_head = head->right;
+    head->right = new_head->left;
+    new_head->left = head;
+    FixHeight(head);
+    FixHeight(new_head);
+    return new_head;
+  }
+
+  static Node* Balance(Node* node) {
+    FixHeight(node);
+    int factor = GetFactor(node);
+    if (factor == 2) {
+      if (GetFactor(node->right) < 0) {
+        node->right = RotateRight(node->right);
+      }
+      return RotateLeft(node);
+    }
+    if (factor == -2) {
+      if (GetFactor(node->left) > 0) {
+        node->left = RotateLeft(node->left);
+      }
+      return RotateRight(node);
+    }
+    return node;
+  }
+
+  static Node* InsertImpl(Node* node, const Key& key, const Value& val) {
+    if (!node) return new Node{key, val};
+    if (node->key < key) {
+      node->right = InsertImpl(node->right, key, val);
+    } else if (key < node->key) {
+      node->left = InsertImpl(node->left, key, val);
+    } else {
+      return node;
+    }
+    return Balance(node);
+  }
+
+  static const Node* NextImpl(const Node* node, const Key& key) {
+    const Node* last_left_turn = nullptr;
+    while (node) {
+      if (key < node->key) {
+        last_left_turn = node;
+        node = node->left;
+      } else if (node->key < key) {
+        node = node->right;
+      } else {
+        if (!node->right) {
+          return last_left_turn;
+        } else {
+          return FindTreeMin(node->right);
+        }
+      }
+    }
+    return last_left_turn;
+  }
+
+  static const Node* PrevImpl(const Node* node, const Key& key) {
+    const Node* last_right_turn = nullptr;
+    while (node) {
+      if (key < node->key) {
+        node = node->left;
+      } else if (node->key < key) {
+        last_right_turn = node;
+        node = node->right;
+      } else {
+        if (!node->left) {
+          return last_right_turn;
+        } else {
+          return FindTreeMax(node->left);
+        }
+      }
+    }
+    return last_right_turn;
+  }
+
+  static Node* FindTreeMin(Node* tree_head) {
+    assert(tree_head);
+    while (tree_head->left) {
+      tree_head = tree_head->left;
+    }
+    return tree_head;
+  }
+
+  static Node* FindTreeMax(Node* tree_head) {
+    assert(tree_head);
+    while (tree_head->right) {
+      tree_head = tree_head->right;
+    }
+    return tree_head;
+  }
+
+  static Node* RemoveMin(Node* node) {
+    if (!node->left) return node->right;
+    node->left = RemoveMin(node->left);
+    return Balance(node);
+  }
+
+  static Node* DeleteImpl(Node* node, const Key& key) {
+    if (!node) return nullptr;
+    if (key < node->key) {
+      node->left = DeleteImpl(node->left, key);
+    } else if (node->key < key) {
+      node->right = DeleteImpl(node->right, key);
+    } else {
+      Node* left = node->left;
+      Node* right = node->right;
+      delete node;
+      if (!right) return left;
+      Node* min = FindTreeMin(right);
+      min->right = RemoveMin(right);
+      min->left = left;
+      return Balance(min);
+    }
+    return Balance(node);
+  }
+
+  Node* head_ = nullptr;
+};
+
+#endif
