58ea5693fca44fee01927f6e143b36a49e21641e
bogdan-lab <lakatosh@phystech.edu>
2022-04-14 20:51:51 +0300

    Add memo, still TL error. Need better compare

--- Tasks/LC_task_30.cpp
+++ Tasks/LC_task_30.cpp
@@ -1,36 +1,52 @@
 #include <algorithm>
+#include <functional>
 #include <iostream>
 #include <string>
 #include <string_view>
+#include <unordered_map>
 #include <vector>
 
-class Solution {
-  struct Key {
-    Key(int pos, std::vector<std::string_view> wds) {
-      input_size = pos;
-      std::sort(wds.begin(), wds.end());
-      words_left = wds;
-    }
-
-    int input_size = 0;
-    std::vector<std::string_view> words_left;
-  };
-
-  friend bool operator<(const Key& lhs, const Key& rhs) {
-    if (lhs.input_size != rhs.input_size) {
-      return lhs.input_size < rhs.input_size;
-    }
-    if (lhs.words_left.size() != rhs.words_left.size()) {
-      return lhs.words_left.size() < rhs.words_left.size();
-    }
-    for (size_t i = 0; i < lhs.words_left.size(); ++i) {
-      if (lhs.words_left[i] != rhs.words_left[i]) {
-        return lhs.words_left[i] < rhs.words_left[i];
-      }
-    }
-    return false;
+struct Key {
+  Key(size_t g_size, std::vector<std::string_view> wds)
+      : input_size(g_size), words_left(std::move(wds)) {
+    std::sort(words_left.begin(), words_left.end());
   }
 
+  size_t input_size = 0;
+  std::vector<std::string_view> words_left;
+};
+
+bool operator==(const Key& lhs, const Key& rhs) {
+  if (lhs.input_size != rhs.input_size) {
+    return false;
+  }
+  if (lhs.words_left.size() != rhs.words_left.size()) {
+    return false;
+  }
+  for (size_t i = 0; i < lhs.words_left.size(); ++i) {
+    if (lhs.words_left[i] != rhs.words_left[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+bool operator!=(const Key& lhs, const Key& rhs) { return !(lhs == rhs); }
+
+struct HashKey {
+  size_t operator()(const Key& k) const {
+    std::hash<size_t> int_hash;
+    std::hash<std::string_view> sv_hash;
+
+    size_t result = int_hash(k.input_size);
+    for (const auto& el : k.words_left) {
+      result ^= sv_hash(el);
+    }
+    return result;
+  }
+};
+
+class Solution {
   bool StartsWith(std::string_view input, std::string_view word) {
     if (input.size() < word.size()) return false;
     for (size_t i = 0; i < word.size(); ++i) {
@@ -41,15 +57,16 @@ class Solution {
     return true;
   }
 
-  void FindSubstringImpl(std::vector<int>& result, int index,
-                         std::string_view input,
-                         std::vector<std::string_view> words,
-                         int initial_words_size) {
-    if (input.empty()) return;
+  std::vector<int> FindSubstringImpl(
+      std::unordered_map<Key, std::vector<int>, HashKey>& memo, int index,
+      std::string_view input, const std::vector<std::string_view>& words,
+      int initial_words_size) {
+    std::vector<int> result;
     if (words.empty()) {
       result.push_back(index);
-      return;
+      return result;
     }
+    if (input.empty()) return result;
     for (size_t i = 0; i < words.size(); ++i) {
       if (StartsWith(input, words[i])) {
         std::vector<std::string_view> words_copy = words;
@@ -57,28 +74,49 @@ class Solution {
         words_copy.pop_back();
         std::string_view input_copy = input;
         input_copy.remove_prefix(words[i].size());
-        FindSubstringImpl(result, index, input_copy, words_copy,
-                          initial_words_size);
+        Key k{input.size(), words_copy};
+        auto it = memo.find(k);
+        if (it == memo.end()) {
+          it = memo
+                   .insert(std::make_pair(
+                       k, FindSubstringImpl(memo, index, input_copy, words_copy,
+                                            initial_words_size)))
+                   .first;
+        }
+        for (const auto& el : it->second) {
+          result.push_back(el);
+        }
       }
     }
     if (words.size() == initial_words_size) {
       index++;
       input.remove_prefix(1);
-      FindSubstringImpl(result, index, input, words, initial_words_size);
+      Key k(input.size(), words);
+      auto it = memo.find(k);
+      if (it == memo.end()) {
+        it = memo.insert(std::make_pair(
+                             k, FindSubstringImpl(memo, index, input, words,
+                                                  initial_words_size)))
+                 .first;
+      }
+      for (const auto& el : it->second) {
+        result.push_back(el);
+      }
     }
+    return result;
   }
 
  public:
   std::vector<int> findSubstring(std::string s,
                                  std::vector<std::string>& words) {
-    std::vector<int> result;
-
     std::vector<std::string_view> words_view;
     words_view.reserve(words.size());
     for (const auto& word : words) {
       words_view.push_back(word);
     }
-    FindSubstringImpl(result, 0, s, words_view, words.size());
+    std::unordered_map<Key, std::vector<int>, HashKey> memo;
+    std::vector<int> result =
+        FindSubstringImpl(memo, 0, s, words_view, words.size());
     std::sort(result.begin(), result.end());
     result.erase(std::unique(result.begin(), result.end()), result.end());
     return result;
