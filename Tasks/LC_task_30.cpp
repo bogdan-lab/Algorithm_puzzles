361f4f0c4d0e4216295422d2ccb1d6b8ef31ca5c
bogdan-lab <lakatosh@phystech.edu>
2022-04-16 17:28:25 +0300

    Change solution logic
    The only problem left is how to solve situation when we have same names
    in the word list

--- Tasks/LC_task_30.cpp
+++ Tasks/LC_task_30.cpp
@@ -1,196 +1,147 @@
 #include <algorithm>
-#include <functional>
+#include <cassert>
 #include <iostream>
-#include <numeric>
+#include <queue>
 #include <string>
 #include <string_view>
-#include <unordered_map>
 #include <vector>
 
-class WordCollection {
- public:
-  WordCollection(const std::vector<std::string>& data) {
-    str_.reserve(data.size());
-    uint16_t count = 0;
-    for (const auto& el : data) {
-      str_.push_back(el);
+std::queue<size_t> FindWord(std::string_view input, std::string_view word) {
+  std::queue<size_t> result;
+  size_t pos = 0;
+  while (true) {
+    size_t new_pos = input.find(word, pos);
+    if (new_pos == std::string_view::npos) {
+      break;
     }
-    std::sort(str_.begin(), str_.end());
-    ids_.resize(data.size());
-    std::iota(ids_.begin(), ids_.end(), static_cast<uint16_t>(0));
+    result.push(new_pos);
+    pos = new_pos + 1;
   }
-
-  const std::vector<std::string_view>& GetStr() const { return str_; }
-
-  const std::vector<uint16_t>& GetIds() const { return ids_; }
-
-  bool Empty() const { return str_.empty(); }
-
-  size_t Size() const { return ids_.size(); }
-
-  void Pop(size_t index) {
-    std::swap(str_[index], str_.back());
-    std::swap(ids_[index], ids_.back());
-    str_.pop_back();
-    ids_.pop_back();
-    std::sort(str_.begin(), str_.end());
-    std::sort(ids_.begin(), ids_.end());
-  }
-
- private:
-  std::vector<std::string_view> str_;
-  std::vector<uint16_t> ids_;
-};
-
-struct Key {
-  Key(size_t g_size, WordCollection wds)
-      : input_size(g_size), words_left(std::move(wds)) {}
-
-  size_t input_size = 0;
-  WordCollection words_left;
-};
-
-bool operator==(const Key& lhs, const Key& rhs) {
-  if (lhs.input_size != rhs.input_size) {
-    return false;
-  }
-  const auto& lhs_ids = lhs.words_left.GetIds();
-  const auto& rhs_ids = rhs.words_left.GetIds();
-  if (lhs_ids.size() != rhs_ids.size()) {
-    return false;
-  }
-  for (size_t i = 0; i < lhs_ids.size(); ++i) {
-    if (lhs_ids[i] != rhs_ids[i]) {
-      return false;
-    }
-  }
-  return true;
+  return result;
 }
 
-bool operator!=(const Key& lhs, const Key& rhs) { return !(lhs == rhs); }
+std::vector<std::queue<size_t>> GetWordsStartPos(
+    std::string_view input, const std::vector<std::string_view>& words) {
+  std::vector<std::queue<size_t>> res;
+  res.reserve(words.size());
+  for (const auto& el : words) {
+    res.push_back(FindWord(input, el));
+  }
+  return res;
+}
 
-struct HashKey {
-  size_t operator()(const Key& k) const {
-    std::hash<size_t> int_hash;
-    size_t result = int_hash(k.input_size);
-    for (const auto& el : k.words_left.GetIds()) {
-      result ^= int_hash(el);
-    }
-    return result;
+struct WordPos {
+  size_t id;
+  size_t pos;
+};
+
+struct PriorityCompare {
+  bool operator()(const WordPos& lhs, const WordPos& rhs) const {
+    return lhs.pos > rhs.pos;
   }
 };
 
 class Solution {
-  bool StartsWith(std::string_view input, std::string_view word) {
-    if (input.size() < word.size()) return false;
-    for (size_t i = 0; i < word.size(); ++i) {
-      if (input[i] != word[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  std::vector<int> FindSubstringImpl(
-      std::unordered_map<Key, std::vector<int>, HashKey>& memo, int index,
-      std::string_view input, const WordCollection& words,
-      int initial_words_size) {
-    std::vector<int> result;
-    if (words.Empty()) {
-      result.push_back(index);
-      return result;
-    }
-    if (input.empty()) return result;
-    const auto& words_str = words.GetStr();
-    for (size_t i = 0; i < words_str.size(); ++i) {
-      if (StartsWith(input, words_str[i])) {
-        WordCollection words_copy = words;
-        words_copy.Pop(i);
-        std::string_view input_copy = input;
-        input_copy.remove_prefix(words_str[i].size());
-        Key k{input.size(), words_copy};
-        auto it = memo.find(k);
-        if (it == memo.end()) {
-          it = memo
-                   .insert(std::make_pair(
-                       k, FindSubstringImpl(memo, index, input_copy, words_copy,
-                                            initial_words_size)))
-                   .first;
-        }
-        for (const auto& el : it->second) {
-          result.push_back(el);
-        }
-      }
-    }
-    if (words.Size() == initial_words_size) {
-      index++;
-      input.remove_prefix(1);
-      Key k(input.size(), words);
-      auto it = memo.find(k);
-      if (it == memo.end()) {
-        it = memo.insert(std::make_pair(
-                             k, FindSubstringImpl(memo, index, input, words,
-                                                  initial_words_size)))
-                 .first;
-      }
-      for (const auto& el : it->second) {
-        result.push_back(el);
-      }
-    }
-    return result;
-  }
-
  public:
   std::vector<int> findSubstring(std::string s,
                                  std::vector<std::string>& words) {
-    WordCollection words_col(words);
-    std::unordered_map<Key, std::vector<int>, HashKey> memo;
-    std::vector<int> result =
-        FindSubstringImpl(memo, 0, s, words_col, words.size());
-    std::sort(result.begin(), result.end());
-    result.erase(std::unique(result.begin(), result.end()), result.end());
+    std::vector<std::string_view> word_views;
+    word_views.reserve(words.size());
+    for (const auto& el : words) {
+      word_views.push_back(el);
+    }
+    std::string_view s_view = s;
+    std::vector<std::queue<size_t>> word_map =
+        GetWordsStartPos(s_view, word_views);
+
+    std::vector<int> result;
+    for (const auto& el : word_map) {
+      if (el.empty()) return result;
+    }
+    std::priority_queue<WordPos, std::vector<WordPos>, PriorityCompare>
+        map_queue;
+
+    for (size_t i = 0; i < word_map.size(); ++i) {
+      map_queue.push({i, word_map[i].front()});
+      word_map[i].pop();
+    }
+
+    std::vector<int> word_is_present(word_views.size());
+    std::queue<WordPos> picked_queue;
+    size_t next_index = 0;
+    while (!map_queue.empty()) {
+      const WordPos& top = map_queue.top();
+      if (top.pos == next_index) {
+        // add to the current picked_queue
+        if (word_is_present[top.id]) {
+          // delete all chosen words before it and itself
+          size_t curr_id = picked_queue.front().id;
+          while (curr_id != top.id) {
+            word_is_present[curr_id] = 0;
+            picked_queue.pop();
+            curr_id = picked_queue.front().id;
+          }
+          word_is_present[top.id] = 0;
+          picked_queue.pop();  // deleted old top.id
+        }
+      } else {
+        // found substring that does not match any word -> free picked_queue
+        while (!picked_queue.empty()) {
+          word_is_present[picked_queue.front().id] = 0;
+          picked_queue.pop();
+        }
+      }
+      word_is_present[top.id] = 1;
+      picked_queue.push(top);
+      // Finalize step - guarantee map_queu reduction, update next index and
+      // check for success
+      if (picked_queue.size() == word_views.size()) {
+        result.push_back(picked_queue.front().pos);
+      }
+      next_index = picked_queue.back().pos + word_views.front().size();
+      if (!word_map[top.id].empty()) {
+        WordPos new_el{top.id, word_map[top.id].front()};
+        word_map[top.id].pop();
+        map_queue.push(new_el);
+      }
+      map_queue.pop();
+    }
     return result;
   }
 };
 
-void PrintRes(const std::vector<int>& res) {
-  std::cout << "res = [";
-  for (const auto& el : res) {
-    std::cout << el << ", ";
-  }
-  std::cout << "]\n";
-}
-
 int main() {
-  Solution test;
   {
-    std::string input = "barfoothefoobarman";
+    Solution test;
+    std::string str = "barfoothefoobarman";
     std::vector<std::string> words{"foo", "bar"};
-    std::vector<int> res = test.findSubstring(input, words);
-    PrintRes(res);
-    std::cout << "expected = [0, 9]\n";
+    std::vector<int> expected{0, 9};
+    auto result = test.findSubstring(str, words);
+    assert(result == expected);
   }
   {
-    std::string input = "barfoofoobarthefoobarman";
+    Solution test;
+    std::string str = "wordgoodgoodgoodbestword";
+    std::vector<std::string> words{"woord", "good", "best", "word"};
+    std::vector<int> expected{};
+    auto result = test.findSubstring(str, words);
+    assert(result == expected);
+  }
+  {
+    Solution test;
+    std::string str = "barfoofoobarthefoobarman";
     std::vector<std::string> words{"bar", "foo", "the"};
-    std::vector<int> res = test.findSubstring(input, words);
-    PrintRes(res);
-    std::cout << "expected = [6, 9, 12]\n";
+    std::vector<int> expected{6, 9, 12};
+    auto result = test.findSubstring(str, words);
+    assert(result == expected);
   }
   {
-    std::string input = "wordgoodgoodgoodbestword";
-    std::vector<std::string> words{"word", "good", "best", "word"};
-    std::vector<int> res = test.findSubstring(input, words);
-    PrintRes(res);
-    std::cout << "expected = []\n";
-  }
-  {
-    std::string input = "wordgoodgoodgoodbestword";
+    Solution test;
+    std::string str = "wordgoodgoodgoodbestword";
     std::vector<std::string> words{"word", "good", "best", "good"};
-    std::vector<int> res = test.findSubstring(input, words);
-    PrintRes(res);
-    std::cout << "expected = [8]\n";
+    std::vector<int> expected{8};
+    auto result = test.findSubstring(str, words);
+    assert(result == expected);
   }
-
   return 0;
 }
