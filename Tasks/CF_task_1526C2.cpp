6da41f8d1eda38ebd28d2c56d8343bda49699893
bogdan-lab <lakatosh@phystech.edu>
2022-04-20 22:38:04 +0300

    Finished segment tree realization
    Need to fix GetRangeNode function

--- Tasks/CF_task_1526C2.cpp
+++ Tasks/CF_task_1526C2.cpp
@@ -9,7 +9,11 @@ constexpr int64_t kEmptyValue = std::numeric_limits<int64_t>::max();
 
 struct Node {
   int max_drop = kEmptyDrop;
+  size_t max_drop_id = std::numeric_limits<size_t>::max();
   int64_t health_sum = kEmptyValue;
+  size_t begin = std::numeric_limits<size_t>::max();
+  // end is not included in the range
+  size_t end = std::numeric_limits<size_t>::min();
 };
 
 void Solution(std::istream& input = std::cin);
@@ -23,6 +27,15 @@ size_t Right(size_t curr_idx);
 size_t Parent(size_t curr_idx);
 
 int64_t SummarizeHealth(int64_t lhs, int64_t rhs);
+Node GetRangeNode(const std::vector<Node>& seg_tree, size_t head, size_t begin,
+                  size_t end);
+Node AddNodes(const Node& lhs, const Node& rhs);
+
+void DeleteElement(std::vector<Node>& seg_tree, size_t id);
+
+bool IsIndexInside(size_t index, size_t begin, size_t end);
+
+size_t CountPotions(const std::vector<Node>& seg_tree);
 
 int main() {
   std::ios_base::sync_with_stdio(false);
@@ -40,12 +53,14 @@ void Solution(std::istream& input) {
     input >> el;
   }
   std::vector<Node> seg_tree = BuildSegmentTree(data);
-  // TODO iterate over indexes and check if health_cum for the seg_tree is
-  // lower_than 0
-  // If it is, than throw away max_drop elements from the subtree
-  // until it becomes positive
-  // move further
-  std::cout << "To be continued....\n";
+  for (size_t end = 1; end < n + 1; ++end) {
+    Node curr_node = GetRangeNode(seg_tree, 0, 0, end);
+    while (curr_node.health_sum < 0) {
+      DeleteElement(seg_tree, curr_node.max_drop_id);
+      curr_node = GetRangeNode(seg_tree, 0, 0, end);
+    }
+  }
+  std::cout << CountPotions(seg_tree) << '\n';
 }
 
 size_t GetBotLevelSize(size_t data_size) {
@@ -62,14 +77,75 @@ std::vector<Node> BuildSegmentTree(const std::vector<int>& data) {
   for (size_t i = 0, j = bot_size - 1; i < data.size(); ++i, ++j) {
     result[j].health_sum = data[i];
     result[j].max_drop = data[i];
+    result[j].max_drop_id = j;
+    result[j].begin = i;
+    result[j].end = i + 1;
   }
   for (size_t i = result.size() - 1; i > 0; i -= 2) {
-    size_t p_i = Parent(i);
-    result[p_i].health_sum =
-        SummarizeHealth(result[i].health_sum, result[i - 1].health_sum);
-    result[p_i].max_drop = std::min(result[i].max_drop, result[i - 1].max_drop);
+    result[Parent(i)] = AddNodes(result[i], result[i - 1]);
   }
   return result;
+}
+
+Node AddNodes(const Node& lhs, const Node& rhs) {
+  Node result;
+  result.health_sum = SummarizeHealth(lhs.health_sum, rhs.health_sum);
+  if (lhs.max_drop < rhs.max_drop) {
+    result.max_drop = lhs.max_drop;
+    result.max_drop_id = lhs.max_drop_id;
+  } else if (lhs.max_drop > rhs.max_drop) {
+    result.max_drop = rhs.max_drop;
+    result.max_drop_id = rhs.max_drop_id;
+  } else {
+    result.max_drop = rhs.max_drop;
+    result.max_drop_id = std::min(rhs.max_drop_id, lhs.max_drop_id);
+  }
+  result.begin = std::min(lhs.begin, rhs.begin);
+  result.end = std::max(lhs.end, rhs.end);
+  return result;
+}
+
+bool IsRangeInside(size_t rng_begin, size_t rng_end, size_t begin, size_t end) {
+  return begin <= rng_begin && rng_end <= end;
+}
+
+Node GetRangeNode(const std::vector<Node>& seg_tree, size_t head, size_t begin,
+                  size_t end) {
+  if (seg_tree[head].begin >= end || seg_tree[head].end <= begin) {
+    return {};
+  }
+  if (seg_tree[head].begin == begin && seg_tree[head].end == end) {
+    return seg_tree[head];
+  }
+  size_t li = Left(head);
+  if (IsRangeInside(begin, end, seg_tree[li].begin, seg_tree[li].end)) {
+    return GetRangeNode(seg_tree, li, begin, end);
+  }
+  size_t ri = Right(head);
+  if (IsRangeInside(begin, end, seg_tree[ri].begin, seg_tree[ri].end)) {
+    return GetRangeNode(seg_tree, ri, begin, end);
+  }
+  // Here we have crossection and do not entirely fit in any subtree
+  return AddNodes(GetRangeNode(seg_tree, li, begin, seg_tree[li].end),
+                  GetRangeNode(seg_tree, ri, seg_tree[ri].begin, end));
+}
+
+void DeleteElement(std::vector<Node>& seg_tree, size_t id) {
+  seg_tree[id] = Node{};
+  while (id != 0) {
+    id = Parent(id);
+    seg_tree[id] = AddNodes(seg_tree[Left(id)], seg_tree[Right(id)]);
+  }
+}
+
+size_t CountPotions(const std::vector<Node>& seg_tree) {
+  size_t count = 0;
+  for (size_t i = (seg_tree.size() + 1) / 2; i < seg_tree.size(); ++i) {
+    if (seg_tree[i].health_sum != kEmptyValue) {
+      count++;
+    }
+  }
+  return count;
 }
 
 int64_t SummarizeHealth(int64_t lhs, int64_t rhs) {
@@ -88,6 +164,14 @@ void RunTests() {
     std::stringstream ss;
     ss << R"(6
 4 -4 1 -3 1 -3
+)";
+    Solution(ss);
+    std::cout << "expected = 5\n";
+  }
+  {
+    std::stringstream ss;
+    ss << R"(12
+-3 -3 -7 -7 -1 -7 3 3 -2 -1 0 -7
 )";
     Solution(ss);
     std::cout << "expected = 5\n";
