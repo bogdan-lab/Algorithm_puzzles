c0f66c71a632388e2dca62b4091dfb8334b0092a
bogdan-lab <lakatosh@phystech.edu>
2022-04-21 19:09:04 +0300

    Commit before moving back to DP

--- Tasks/CF_1669_task_D.cpp
+++ Tasks/CF_1669_task_D.cpp
@@ -1,10 +1,14 @@
+#include <algorithm>
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <string_view>
 
 void Solution(std::istream& input = std::cin);
 void RunTests();
-bool CheckPainting(const std::string& target, std::string source);
+bool CheckPainting(std::string_view input);
+std::vector<std::string_view> SplitByW(std::string_view input);
+bool CheckSplitedPart(std::string_view part);
 
 int main() {
   std::ios_base::sync_with_stdio(false);
@@ -26,39 +30,37 @@ void Solution(std::istream& input) {
   }
 }
 
-std::string Print(const std::string& str, const std::string& stamp,
-                  size_t pos) {
-  std::string res = str;
-  res[pos] = stamp[0];
-  res[pos + 1] = stamp[1];
-  return res;
+bool CheckSplitedPart(std::string_view part) {
+  if (part.size() == 1) return false;
+  return part.front() != part[1] && part.back() != part[part.size() - 1];
 }
 
-bool CheckPainting(const std::string& target, std::string source) {
-  if (target == source) return true;
-  bool result = false;
-  for (size_t i = 0; i < target.size(); ++i) {
-    if (target[i] != source[i]) {
-      if (i == 0) {
-        std::string lhs = Print(source, "BR", i);
-        std::string rhs = Print(source, "RB", i);
-        result =
-            result || CheckPainting(target, lhs) || CheckPainting(target, rhs);
-      } else if (i == target.size() - 1) {
-        std::string lhs = Print(source, "BR", i - 1);
-        std::string rhs = Print(source, "RB", i - 1);
-        result =
-            result || CheckPainting(target, lhs) || CheckPainting(target, rhs);
-      } else {
-        std::string lhs1 = Print(source, "BR", i);
-        std::string lhs2 = Print(source, "BR", i - 1);
-        std::string rhs1 = Print(source, "RB", i);
-        std::string rhs2 = Print(source, "RB", i - 1);
-        result = result || CheckPainting(target, lhs1) ||
-                 CheckPainting(target, rhs1) || CheckPainting(target, lhs1) ||
-                 CheckPainting(target, rhs2);
-      }
+std::vector<std::string_view> SplitByW(std::string_view input) {
+  size_t pos = 0;
+  std::vector<std::string_view> result;
+  input.remove_prefix(std::min(input.find_first_not_of("W"), input.size()));
+  while (!input.empty() && input.back() == 'W') {
+    input.remove_suffix(1);
+  }
+  while (pos < input.size()) {
+    size_t next_pos = input.find('W', pos = pos);
+    result.push_back(input.substr(pos, next_pos - pos));
+    if (next_pos == std::string_view::npos) {
+      break;
     }
+    while (next_pos < input.size() && input[next_pos] == 'W') {
+      next_pos++;
+    }
+    pos = next_pos;
+  }
+  return result;
+}
+
+bool CheckPainting(std::string_view target) {
+  std::vector<std::string_view> splitted = SplitByW(target);
+  bool result = true;
+  for (const auto& el : splitted) {
+    result = result && CheckSplitedPart(el);
   }
   return result;
 }
@@ -66,7 +68,32 @@ bool CheckPainting(const std::string& target, std::string source) {
 void RunTests() {
   {
     std::stringstream ss;
-    ss << R"()";
+    ss << R"(12
+5
+BRBBW
+1
+B
+2
+WB
+2
+RW
+3
+BRB
+3
+RBB
+7
+WWWWWWW
+9
+RBWBWRRBW
+10
+BRBRBRBRRB
+12
+BBBRWWRRRWBR
+10
+BRBRBRBRBW
+5
+RBWBW
+)";
     Solution(ss);
     std::cout << "expected = 0\n";
   }
