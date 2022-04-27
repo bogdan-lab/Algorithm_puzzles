d9b33e239159436089679cf324a4cf91429db161
bogdan-lab <lakatosh@phystech.edu>
2022-04-27 20:34:24 +0300

    Done

--- Tasks/LC_task_32.cpp
+++ Tasks/LC_task_32.cpp
@@ -3,48 +3,55 @@
 #include <limits>
 #include <stack>
 #include <string>
+#include <vector>
 
-void ClearStack(std::stack<size_t>& data) {
-  std::stack<size_t> tmp;
-  data.swap(tmp);
+constexpr size_t kEmptyValue = std::numeric_limits<size_t>::max();
+constexpr int kEmptyLength = std::numeric_limits<int>::min();
+
+// Result will contain end index for valid bracets substring starting in i-th
+// position. If kEmptyValue is stored in i-th position, then there is no correct
+// bracet surface starting from it
+std::vector<size_t> CreateChainVector(std::string_view str) {
+  std::vector<size_t> result(str.size(), kEmptyValue);
+
+  std::stack<size_t> pos_buff;
+  for (size_t i = 0; i < str.size(); ++i) {
+    if (str[i] == '(') {
+      pos_buff.push(i);
+    } else {
+      if (!pos_buff.empty()) {
+        result[pos_buff.top()] = i + 1;
+        pos_buff.pop();
+      }
+    }
+  }
+  return result;
+}
+
+int GetChainLen(const std::vector<size_t>& chain, size_t i,
+                std::vector<int>& memo) {
+  if (i >= memo.size()) return 0;
+  if (memo[i] == kEmptyLength) {
+    if (chain[i] == kEmptyValue) {
+      memo[i] = 0;
+    } else {
+      memo[i] = chain[i] - i + GetChainLen(chain, chain[i], memo);
+    }
+  }
+  return memo[i];
 }
 
 class Solution {
  public:
   int longestValidParentheses(std::string s) {
     if (s.empty()) return 0;
-    std::stack<size_t> open_pos;
-    int max_len = 0;
-    size_t start_pos = s.find('(');
-    size_t i = start_pos;
-    // end points to the one after!
-    size_t end_pos = start_pos;
-    bool first_match_found = false;
-    while (i < s.size()) {
-      if (s[i] == '(') {
-        open_pos.push(i++);
-      } else {
-        if (open_pos.empty()) {
-          max_len = std::max<int>(max_len, end_pos - start_pos);
-          start_pos = s.find('(', i);
-          if (start_pos == std::string::npos) {
-            return max_len;
-          }
-          ClearStack(open_pos);
-          open_pos.push(start_pos);
-          i = start_pos + 1;
-          first_match_found = false;
-        } else {
-          start_pos = first_match_found ? std::min(start_pos, open_pos.top())
-                                        : open_pos.top();
-          end_pos = i + 1;
-          open_pos.pop();
-          i++;
-          first_match_found = true;
-        }
-      }
+    std::vector<size_t> chain = CreateChainVector(s);
+    std::vector<int> memo(s.size(), kEmptyLength);
+    int max_len = std::numeric_limits<int>::min();
+    for (size_t i = 0; i < chain.size(); ++i) {
+      max_len = std::max(max_len, GetChainLen(chain, i, memo));
     }
-    return std::max<int>(max_len, end_pos - start_pos);
+    return max_len;
   }
 };
 
