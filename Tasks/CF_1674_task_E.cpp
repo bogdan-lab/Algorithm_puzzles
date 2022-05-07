b6c9f6a970b72f8f610cd868a271e110af7f4958
bogdan-lab <lakatosh@phystech.edu>
2022-05-07 12:44:01 +0300

    Wrong answer test 32

--- Tasks/CF_1674_task_E.cpp
+++ Tasks/CF_1674_task_E.cpp
@@ -4,8 +4,25 @@
 #include <sstream>
 #include <vector>
 
-int DestroyWall(int index, std::vector<int>& health);
-int CalcShots(int index, std::vector<int> health);
+constexpr int kMaxValue = 1'000'001;
+
+struct Health {
+  Health(int g_health) {
+    if (g_health <= 0) return;
+    health = g_health;
+    count_c_hits = (health + 1) / 2;
+  }
+
+  int health = 0;
+  int count_c_hits = 0;
+};
+
+int SolveForTriplet(const std::vector<Health>& data, size_t index,
+                    const std::vector<int>& pref_min_c_counts);
+
+int SimpleShotCount(Health first, Health second);
+int ShotParallel(Health first, Health second);
+std::vector<int> GetPrefixMinCCounts(const std::vector<Health>& data);
 
 void Solution(std::istream& input = std::cin);
 void RunTests();
@@ -13,51 +30,66 @@ void RunTests();
 int main() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
-  RunTests();
-  // Solution(std::cin);
+  // RunTests();
+  Solution(std::cin);
   return 0;
 }
 
 void Solution(std::istream& input) {
   int n;
   input >> n;
-  std::vector<int> health(n);
-  for (auto& el : health) {
-    input >> el;
+  std::vector<Health> data;
+  data.reserve(n);
+  while (n--) {
+    int tmp = 0;
+    input >> tmp;
+    data.push_back(Health(tmp));
   }
-
-  int min_count = std::numeric_limits<int>::max();
-  for (int i = 0; i < health.size(); ++i) {
-    min_count = std::min(min_count, CalcShots(i, health));
+  int min_val = std::min({SimpleShotCount(data[0], data[1]),
+                          SimpleShotCount(data[1], data[0]),
+                          ShotParallel(data[0], data[1])});
+  // pref_...[i] stores min counts for all values before i
+  std::vector<int> pref_min_c_counts = GetPrefixMinCCounts(data);
+  for (size_t i = 0; i < data.size() - 2; ++i) {
+    min_val = std::min(min_val, SolveForTriplet(data, i, pref_min_c_counts));
   }
-  std::cout << min_count << '\n';
+  std::cout << min_val << '\n';
 }
 
-int DestroyWall(int index, std::vector<int>& health) {
-  int count = (health[index] + 1) / 2;
-  health[index] -= count * 2;
-  if (index < health.size() - 1) {
-    health[index + 1] -= count;
-  }
-  if (index > 1) {
-    health[index - 1] -= count;
-  }
-  return count;
+int SolveForTriplet(const std::vector<Health>& data, size_t index,
+                    const std::vector<int>& pref_min_c_counts) {
+  return std::min(
+      {SimpleShotCount(data[index], data[index + 1]),
+       SimpleShotCount(data[index + 1], data[index]),
+       ShotParallel(data[index + 1], data[index]),
+       /*2->X=*/std::max(data[index].health, data[index + 2].health),
+       SimpleShotCount(data[index + 1], data[index + 2]),
+       SimpleShotCount(data[index + 2], data[index + 1]),
+       ShotParallel(data[index + 2], data[index + 1]),
+       data[index].count_c_hits + pref_min_c_counts[index],
+       data[index + 1].count_c_hits + pref_min_c_counts[index + 1],
+       data[index + 2].count_c_hits + pref_min_c_counts[index + 2]});
 }
 
-int CalcShots(int index, std::vector<int> health) {
-  int count = DestroyWall(index, health);
-  auto st_it =
-      std::find_if(health.begin(), health.end(), [](int el) { return el > 0; });
-  int n_idx = st_it - health.begin();
-  int min_val = *st_it;
-  for (auto it = st_it; it != health.end(); ++it) {
-    if (*it > 0 && *it < min_val) {
-      min_val = *it;
-      n_idx = it - health.begin();
-    }
+int SimpleShotCount(Health first, Health second) {
+  return first.count_c_hits + std::max(0, second.health - first.count_c_hits);
+}
+
+int ShotParallel(Health first, Health second) {
+  int count_pair_shot = std::min(first.health, second.health) / 3;
+  int damage = count_pair_shot * 3;
+  first = Health(first.health - damage);
+  second = Health(second.health - damage);
+  return 2 * count_pair_shot + std::min(SimpleShotCount(first, second),
+                                        SimpleShotCount(second, first));
+}
+
+std::vector<int> GetPrefixMinCCounts(const std::vector<Health>& data) {
+  std::vector<int> res(data.size(), kMaxValue);
+  for (size_t i = 1; i < data.size(); ++i) {
+    res[i] = std::min(res[i - 1], data[i - 1].count_c_hits);
   }
-  return count + DestroyWall(n_idx, health);
+  return res;
 }
 
 void RunTests() {
