7f3e826b36a3baaa28174099f9dd6d2af3c53b5e
bogdan-lab <lakatosh@phystech.edu>
2022-06-07 22:23:01 +0300

    more beautiful solution with DP

--- Tasks/LC_task_42.cpp
+++ Tasks/LC_task_42.cpp
@@ -7,35 +7,11 @@ std::vector<int> GetSuffixMax(const std::vector<int>& data);
 class Solution {
  public:
   int trap(std::vector<int>& height) {
-    int pos = std::max_element(height.begin(), height.end()) - height.begin();
-    int volume = 0;
-    // accumulate to the left
     std::vector<int> prefix = GetPrefixMax(height);
-    int start = pos;
-    while (start > 0) {
-      int end_val = prefix[start - 1];
-      int curr_pos = start;
-      int filled = 0;
-      while (height[curr_pos - 1] != end_val) {
-        --curr_pos;
-        filled += height[curr_pos];
-      }
-      volume += (start - curr_pos) * end_val - filled;
-      start = curr_pos - 1;
-    }
-    // accumulate to the right
     std::vector<int> suffix = GetSuffixMax(height);
-    start = pos;
-    while (start < height.size() - 1) {
-      int end_val = suffix[start + 1];
-      int curr_pos = start;
-      int filled = 0;
-      while (height[curr_pos + 1] != end_val) {
-        ++curr_pos;
-        filled += height[curr_pos];
-      }
-      volume += (curr_pos - start) * end_val - filled;
-      start = curr_pos + 1;
+    int volume = 0;
+    for (size_t i = 0; i < prefix.size(); ++i) {
+      volume += std::min(prefix[i], suffix[i]) - height[i];
     }
     return volume;
   }
