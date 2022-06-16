9836f361d4f1e48b24221c1d1d0b672e78ad091e
bogdan-lab <lakatosh@phystech.edu>
2022-06-16 21:49:38 +0300

    done bottom-up

--- Tasks/LC_task_44.cpp
+++ Tasks/LC_task_44.cpp
@@ -6,14 +6,17 @@ constexpr int kEmptyValue = 2;
 bool Match(const std::string& str, const std::string& ptrn, size_t str_pos,
            size_t ptrn_pos, std::vector<std::vector<int>>& memo);
 
+bool Match(const std::string& str, const std::string& ptrn);
+
 class Solution {
  public:
   bool isMatch(std::string s, std::string p) {
-    std::vector<std::vector<int>> memo(s.size());
-    for (auto& el : memo) {
-      el.resize(p.size(), kEmptyValue);
-    }
-    return Match(s, p, 0, 0, memo);
+    // std::vector<std::vector<int>> memo(s.size());
+    // for (auto& el : memo) {
+    //  el.resize(p.size(), kEmptyValue);
+    //}
+    // return Match(s, p, 0, 0, memo);
+    return Match(s, p);
   }
 };
 
@@ -47,3 +50,33 @@ bool Match(const std::string& str, const std::string& ptrn, size_t str_pos,
   return memo[str_pos][ptrn_pos];
 }
 
+// case bottom-up
+bool Match(const std::string& str, const std::string& ptrn) {
+  std::vector<std::vector<int>> buff(str.size() + 1);
+  for (auto& el : buff) {
+    el.resize(ptrn.size() + 1, 0);
+  }
+  buff[0][0] = 1;
+  for (size_t i = 0; i < ptrn.size(); ++i) {
+    if (ptrn[i] == '*') {
+      buff[0][i + 1] = 1;
+    } else {
+      break;
+    }
+  }
+  for (size_t s_pos = 0; s_pos < str.size(); ++s_pos) {
+    for (size_t p_pos = 0; p_pos < ptrn.size(); ++p_pos) {
+      if (ptrn[p_pos] == str[s_pos] || ptrn[p_pos] == '?') {
+        buff[s_pos + 1][p_pos + 1] = buff[s_pos][p_pos];
+      } else if (ptrn[p_pos] == '*') {
+        buff[s_pos + 1][p_pos + 1] = buff[s_pos][p_pos] |
+                                     buff[s_pos][p_pos + 1] |
+                                     buff[s_pos + 1][p_pos];
+      } else {
+        buff[s_pos + 1][p_pos + 1] = 0;
+      }
+    }
+  }
+
+  return buff[str.size()][ptrn.size()];
+}
