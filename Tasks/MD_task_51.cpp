f3ea386bae30cc5e28760f18169c5c0445073ff2
bogdan-lab <lakatosh@phystech.edu>
2022-06-21 22:47:30 +0300

    Done with cleaning at call stack return

--- Tasks/MD_task_51.cpp
+++ Tasks/MD_task_51.cpp
@@ -3,22 +3,19 @@
 #include <vector>
 
 std::vector<std::string> CreateEmptyBoard(int n);
-std::vector<std::vector<int>> CreateEmptyMap(int n);
-
-void PlaceQueen(std::vector<std::string>& board,
-                std::vector<std::vector<int>>& map, int row, int col);
+bool CheckPos(const std::vector<std::string>& board, int row, int col);
 
 void BuildBoards(std::vector<std::vector<std::string>>& sol_boards,
-                 std::vector<std::string> curr_board,
-                 std::vector<std::vector<int>> curr_map, int curr_row,
+                 std::vector<std::string>& curr_board, int curr_row,
                  int curr_col);
 
 class Solution {
  public:
   std::vector<std::vector<std::string>> solveNQueens(int n) {
     std::vector<std::vector<std::string>> result;
+    std::vector<std::string> initial_board = CreateEmptyBoard(n);
     for (int col = 0; col < n; ++col) {
-      BuildBoards(result, CreateEmptyBoard(n), CreateEmptyMap(n), 0, col);
+      BuildBoards(result, initial_board, 0, col);
     }
     return result;
   }
@@ -28,63 +25,42 @@ std::vector<std::string> CreateEmptyBoard(int n) {
   return std::vector<std::string>(n, std::string(n, '.'));
 }
 
-std::vector<std::vector<int>> CreateEmptyMap(int n) {
-  return std::vector<std::vector<int>>(n, std::vector<int>(n));
-}
-
 void BuildBoards(std::vector<std::vector<std::string>>& sol_boards,
-                 std::vector<std::string> curr_board,
-                 std::vector<std::vector<int>> curr_map, int curr_row,
+                 std::vector<std::string>& curr_board, int curr_row,
                  int curr_col) {
-  if (curr_map[curr_row][curr_col]) {
+  if (!CheckPos(curr_board, curr_row, curr_col)) {
     return;
   }
-  PlaceQueen(curr_board, curr_map, curr_row, curr_col);
+  curr_board[curr_row][curr_col] = 'Q';
   if (curr_row == curr_board.size() - 1) {
-    sol_boards.push_back(std::move(curr_board));
-    return;
+    sol_boards.push_back(curr_board);
+  } else {
+    int new_row = curr_row + 1;
+    for (int i = 0; i < curr_board.size(); ++i) {
+      BuildBoards(sol_boards, curr_board, new_row, i);
+    }
   }
-  ++curr_row;
-  for (int i = 0; i < curr_board.size(); ++i) {
-    BuildBoards(sol_boards, curr_board, curr_map, curr_row, i);
-  }
+  curr_board[curr_row][curr_col] = '.';
 }
 
-void PlaceQueen(std::vector<std::string>& board,
-                std::vector<std::vector<int>>& map, int row, int col) {
-  board[row][col] = 'Q';
-  for (int i = 0; i < map.size(); ++i) {
-    map[row][i] = 1;
+bool CheckPos(const std::vector<std::string>& board, int row, int col) {
+  // I know the order of recursion -> use it
+  for (int i = 0; i < row; ++i) {
+    if (board[i][col] == 'Q') return false;
   }
-  for (int i = 0; i < map.size(); ++i) {
-    map[i][col] = 1;
-  }
-  int curr_row = row;
-  int curr_col = col;
-  while (curr_row < map.size() && curr_col < map.size()) {
-    map[curr_row][curr_col] = 1;
-    ++curr_row;
-    ++curr_col;
-  }
-  curr_row = row;
-  curr_col = col;
+  int curr_row = row - 1;
+  int curr_col = col - 1;
   while (curr_row >= 0 && curr_col >= 0) {
-    map[curr_row][curr_col] = 1;
+    if (board[curr_row][curr_col] == 'Q') return false;
     --curr_row;
     --curr_col;
   }
-  curr_row = row;
-  curr_col = col;
-  while (curr_row >= 0 && curr_col < map.size()) {
-    map[curr_row][curr_col] = 1;
+  curr_row = row - 1;
+  curr_col = col + 1;
+  while (curr_row >= 0 && curr_col < board.size()) {
+    if (board[curr_row][curr_col] == 'Q') return false;
     --curr_row;
     ++curr_col;
   }
-  curr_row = row;
-  curr_col = col;
-  while (curr_row < map.size() && curr_col >= 0) {
-    map[curr_row][curr_col] = 1;
-    ++curr_row;
-    --curr_col;
-  }
+  return true;
 }
