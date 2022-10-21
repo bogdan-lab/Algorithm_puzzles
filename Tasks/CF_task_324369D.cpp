14066cc71c70d66a0daca2baed7453e938e84b29
bogdan-lab <lakatosh@phystech.edu>
2022-10-21 20:59:13 +0300

    Take into account too large jumps

--- Tasks/CF_task_324369D.cpp
+++ Tasks/CF_task_324369D.cpp
@@ -26,24 +26,27 @@ void Solution(std::istream& input) {
     }
 
     int64_t count = 0;
-    int start =
-        std::find_if(data.begin(), data.end(), [](int v) { return v > 1; }) -
-        data.begin();
-    while (start < data.size()) {
-      int next = start;
-      while (next < data.size()) {
-        if (data[next] == 1) {
-          ++next;
-        } else {
-          --data[next];
-          next += data[next] + 1;
+    int start = 0;
+    while (true) {
+      start = std::find_if(data.begin() + start, data.end(),
+                           [](int v) { return v > 1; }) -
+              data.begin();
+      if (start == data.size()) break;
+      if (data[start] + start >= data.size()) {
+        int new_val = std::max<int>(data.size() - 1 - start, 1);
+        count += data[start] - new_val;
+        data[start] = new_val;
+      } else {
+        int next = start;
+        while (next < data.size()) {
+          if (data[next] == 1) {
+            ++next;
+          } else {
+            --data[next];
+            next += data[next] + 1;
+          }
         }
-      }
-      ++count;
-      if (data[start] == 1) {
-        start = std::find_if(data.begin() + start, data.end(),
-                             [](int v) { return v > 1; }) -
-                data.begin();
+        ++count;
       }
     }
 
@@ -54,15 +57,17 @@ void Solution(std::istream& input) {
 void RunTests() {
   {
     std::stringstream ss;
-    ss << R"(3
+    ss << R"(4
 7
 1 4 2 2 2 2 2
 2
 2 3
 5
 1 1 1 1 1
+4
+1 9 2 1
 )";
     Solution(ss);
-    std::cout << "expected = 4; 3; 0\n";
+    std::cout << "expected = 4; 3; 0; 9\n";
   }
 }
