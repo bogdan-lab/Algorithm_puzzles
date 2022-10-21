32fb146290a3759c7e2e511bd5a627d18f4cade3
bogdan-lab <lakatosh@phystech.edu>
2022-10-21 23:53:04 +0300

    Using DP, WA

--- Tasks/CF_task_324369D.cpp
+++ Tasks/CF_task_324369D.cpp
@@ -3,61 +3,74 @@
 #include <sstream>
 #include <vector>
 
+struct DPStruct {
+  int64_t count = 0;
+  int need_out = 0;
+  int curr_in = 0;
+};
+int64_t GetCount(const std::vector<int>& data);
+
 void Solution(std::istream& input = std::cin);
 void RunTests();
 
 int main() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
-  // RunTests();
-  Solution(std::cin);
+  RunTests();
+  // Solution(std::cin);
   return 0;
+}
+
+int64_t GetCount(const std::vector<int>& data) {
+  std::vector<DPStruct> dp(data.size());
+
+  for (int i = 0; i < data.size(); ++i) {
+    dp[i].need_out = data[i] - 1;
+    if (data[i] + i >= data.size()) {
+      dp[i].count = data[i] - std::max<int>(data.size() - 1 - i, 1);
+    }
+  }
+
+  if (data.size() == 1) return dp[0].count;
+
+  for (int i = data.size() - 2; i >= 0; --i) {
+    int64_t curr_c = 0;
+    for (int j = 2; j <= data[i] - dp[i].count; ++j) {
+      int s = i + j;
+      if (dp[s].curr_in >= dp[s].need_out) {
+        ++curr_c;
+      } else {
+        ++dp[s].curr_in;
+      }
+    }
+    dp[i].count += dp[i + 1].count + curr_c;
+  }
+  return dp[0].count;
 }
 
 void Solution(std::istream& input) {
   int t;
   input >> t;
+  std::vector<int> data;
+
   while (t--) {
-    int n;
+    size_t n;
     input >> n;
-    std::vector<int> data(n);
+    data.resize(n);
     for (auto& el : data) {
       input >> el;
     }
 
-    int64_t count = 0;
-    int start = 0;
-    while (true) {
-      start = std::find_if(data.begin() + start, data.end(),
-                           [](int v) { return v > 1; }) -
-              data.begin();
-      if (start == data.size()) break;
-      if (data[start] + start >= data.size()) {
-        int new_val = std::max<int>(data.size() - 1 - start, 1);
-        count += data[start] - new_val;
-        data[start] = new_val;
-      } else {
-        int next = start;
-        while (next < data.size()) {
-          if (data[next] == 1) {
-            ++next;
-          } else {
-            --data[next];
-            next += data[next] + 1;
-          }
-        }
-        ++count;
-      }
-    }
-
-    std::cout << count << '\n';
+    int64_t res = GetCount(data);
+    std::cout << res << '\n';
+    data.clear();
   }
 }
 
 void RunTests() {
   {
     std::stringstream ss;
-    ss << R"(4
+    ss << R"(5
 7
 1 4 2 2 2 2 2
 2
@@ -66,8 +79,10 @@ void RunTests() {
 1 1 1 1 1
 4
 1 9 2 1
+1
+6
 )";
     Solution(ss);
-    std::cout << "expected = 4; 3; 0; 9\n";
+    std::cout << "expected = 4; 3; 0; 9; 5\n";
   }
 }
