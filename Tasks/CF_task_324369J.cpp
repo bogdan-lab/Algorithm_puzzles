dfabd283f0b9a530482293ad005a9a8797746597
bogdan-lab <lakatosh@phystech.edu>
2022-10-28 08:55:28 +0300

    Done

--- Tasks/CF_task_324369J.cpp
+++ Tasks/CF_task_324369J.cpp
@@ -37,16 +37,22 @@ void Solution(std::istream& input) {
   }
 
   std::sort(data.begin(), data.end());
-  std::vector<int64_t> diff(kMaxNum);
+  auto it = std::unique(data.begin(), data.end());
+  data.erase(it, data.end());
+  std::vector<int64_t> diff(kMaxNum + 1);
 
   int fill_num = ws - dl_num;
+  if (fill_num > data.size()) {
+    std::cout << "0\n";
+    return;
+  }
   for (int i = 0; i < data.size() - fill_num + 1; ++i) {
     int64_t dist = data[i + fill_num - 1] - data[i] + 1;
     if (dist <= ws) {
       int last_val = data[i + fill_num - 1];
       int corr = std::min(last_val - ws, 0);
       last_val -= corr;
-      AddRange(diff, last_val, last_val + 1 + ws - dist);
+      AddRange(diff, last_val, data[i] + ws);
     }
   }
 
@@ -56,7 +62,7 @@ void Solution(std::istream& input) {
   }
 
   int count = std::count_if(p_diff.begin(), p_diff.end(),
-                            [](int64_t v) { return v != 0; });
+                            [](int64_t v) { return v > 0; });
   std::cout << count << '\n';
 }
 
@@ -91,6 +97,62 @@ void RunTests() {
     )";
     Solution(ss);
     std::cout << "expected = 2\n";
+  }
+  {
+    std::stringstream ss;
+    ss << R"(3 2 3
+    1 20 40
+    )";
+    Solution(ss);
+    std::cout << "expected = 7\n";
+  }
+  {
+    std::stringstream ss;
+    ss << R"(3 2 3
+    2 20 40
+    )";
+    Solution(ss);
+    std::cout << "expected = 8\n";
+  }
+  {
+    std::stringstream ss;
+    ss << R"(3 1 3
+    2 20 40
+    )";
+    Solution(ss);
+    std::cout << "expected = 0\n";
+  }
+  {
+    std::stringstream ss;
+    ss << R"(4 1 3
+    1 2 20 40
+    )";
+    Solution(ss);
+    std::cout << "expected = 1\n";
+  }
+  {
+    std::stringstream ss;
+    ss << R"(6 1 3
+    1 2 2 2 2 2
+    )";
+    Solution(ss);
+    std::cout << "expected = 1\n";
+  }
+  {
+    std::stringstream ss;
+    ss << R"(5 1 3
+    1 2 3 4 5
+    )";
+    Solution(ss);
+    std::cout << "expected = 4\n";
+  }
+  {
+    std::stringstream ss;
+    ss << R"(5 1 1
+    1 2 3 4 5
+    )";
+    Solution(ss);
+    std::cout << "expected = 1'000'000\n";
   }
   {
     std::stringstream ss;
