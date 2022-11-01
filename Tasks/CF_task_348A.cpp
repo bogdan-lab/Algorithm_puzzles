ca38ee8f416048c12461dac42a64e798821ffd07
bogdan-lab <lakatosh@phystech.edu>
2022-11-01 08:35:40 +0300

    done

--- Tasks/CF_task_348A.cpp
+++ Tasks/CF_task_348A.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <iostream>
+#include <numeric>
 #include <sstream>
 #include <vector>
 
@@ -14,14 +15,6 @@ int main() {
   return 0;
 }
 
-bool AllSame(const std::vector<int64_t>& data) {
-  int64_t v = data.front();
-  for (int i = 1; i < data.size(); ++i) {
-    if (data[i] != v) return false;
-  }
-  return true;
-}
-
 void Solution(std::istream& input) {
   int n;
   input >> n;
@@ -29,15 +22,9 @@ void Solution(std::istream& input) {
   for (auto& el : data) {
     input >> el;
   }
-  if (AllSame(data)) {
-    std::cout << data.front() + 1 << '\n';
-    return;
-  }
-  std::nth_element(data.begin(), data.begin() + 1, data.end());
-  int64_t init_count = *(data.begin() + 1);
-  int64_t min_val = *std::min_element(data.begin(), data.end());
+  int64_t s = std::accumulate(data.begin(), data.end(), 0LL);
   int64_t max_val = *std::max_element(data.begin(), data.end());
-  std::cout << init_count + std::max(max_val - init_count, min_val) << '\n';
+  std::cout << std::max(max_val, (s + n - 2) / (n - 1)) << '\n';
 }
 
 void RunTests() {
