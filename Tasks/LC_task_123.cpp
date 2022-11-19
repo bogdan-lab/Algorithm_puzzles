adfb9d380e0f1335edac32d2637df1a71e1844e7
bogdan-lab <lakatosh@phystech.edu>
2022-11-19 22:54:35 +0300

    avoid creating suffix array

--- Tasks/LC_task_123.cpp
+++ Tasks/LC_task_123.cpp
@@ -5,30 +5,24 @@ class Solution {
  public:
   int maxProfit(std::vector<int>& prices) {
     if (prices.size() == 1) return 0;
-    std::vector<int> prefix(prices.size());
+    std::vector<int> data(prices.size());
     int min_p = prices.front();
     int max_val = 0;
     for (size_t i = 1; i < prices.size(); ++i) {
       max_val = std::max(max_val, prices[i] - min_p);
       min_p = std::min(min_p, prices[i]);
-      prefix[i] = max_val;
+      data[i] = max_val;
     }
-
-    std::vector<int> suffix(prices.size());
+    // Here data[i] stores income on range [0, i]
     int max_p = prices.back();
     max_val = 0;
+    int res = 0;
     for (int i = prices.size() - 2; i >= 0; --i) {
+      res = std::max(res, data[i + 1] + max_val);
       max_val = std::max(max_val, max_p - prices[i]);
       max_p = std::max(max_p, prices[i]);
-      suffix[i] = max_val;
     }
-
-    // Case where we do only 1 the most optimal trade or do not do any trade at
-    // all.
-    int res = prefix.back();
-    for (int i = 1; i < prefix.size() - 1; ++i) {
-      res = std::max(res, prefix[i] + suffix[i + 1]);
-    }
+    res = std::max(res, data[0] + max_val);
     return res;
   }
 };
