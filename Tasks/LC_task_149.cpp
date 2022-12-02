e5e6f8201f6e7fe0c91d3e3c23cd7d31273876d1
bogdan-lab <lakatosh@phystech.edu>
2022-12-02 08:28:21 +0300

    Use cross product

--- Tasks/LC_task_149.cpp
+++ Tasks/LC_task_149.cpp
@@ -2,61 +2,20 @@
 #include <limits>
 #include <vector>
 
-constexpr int kEmptyValue = std::numeric_limits<int>::max();
-
 struct Line {
-  int x_vertical = kEmptyValue;
-  int y_horizontal = kEmptyValue;
-
-  std::vector<int> p0;
-  int step_right = kEmptyValue;
-  int step_up = kEmptyValue;
+  Line(std::vector<int> l, std::vector<int> r)
+      : p1(std::move(l)), p2(std::move(r)) {}
+  std::vector<int> p1;
+  std::vector<int> p2;
 };
 
-int GetGCD(int lhs, int rhs) {
-  if (lhs < rhs) std::swap(lhs, rhs);
-  while (rhs) {
-    lhs %= rhs;
-    std::swap(lhs, rhs);
-  }
-  return lhs;
+int Cross(const std::vector<int>& v1, const std::vector<int>& v2) {
+  return v1[0] * v2[1] - v1[1] * v2[0];
 }
 
-Line GetLine(std::vector<int> lhs, std::vector<int> rhs) {
-  Line res;
-  if (lhs[0] > rhs[0]) std::swap(lhs, rhs);
-  res.p0 = lhs;
-  if (lhs[0] == rhs[0]) {
-    res.x_vertical = lhs[0];
-    return res;
-  }
-  if (lhs[1] == rhs[1]) {
-    res.y_horizontal = lhs[1];
-    return res;
-  }
-
-  res.step_right = rhs[0] - lhs[0];
-  res.step_up = rhs[1] - lhs[1];
-  int gcd = GetGCD(std::abs(res.step_right), std::abs(res.step_up));
-  res.step_right /= gcd;
-  res.step_up /= gcd;
-  return res;
-}
-
-bool IsOnLine(std::vector<int> p0, const Line& l) {
-  if (p0[0] == l.x_vertical) {
-    return true;
-  }
-  if (p0[1] == l.y_horizontal) {
-    return true;
-  }
-  std::vector<int> cp = l.p0;
-  if (p0[0] > cp[0]) std::swap(p0, cp);
-  int right = cp[0] - p0[0];
-  // step_right is always positive!
-  int steps = right / l.step_right;
-  if (right != steps * l.step_right) return false;
-  return steps * l.step_up + p0[1] == cp[1];
+bool IsOnLine(const Line& l, const std::vector<int>& p) {
+  return Cross({p[0] - l.p1[0], p[1] - l.p1[1]},
+               {p[0] - l.p2[0], p[1] - l.p2[1]}) == 0;
 }
 
 class Solution {
@@ -66,14 +25,14 @@ class Solution {
     std::vector<Line> all_lines;
     for (int i = 0; i < points.size(); ++i) {
       for (int j = i + 1; j < points.size(); ++j) {
-        all_lines.push_back(GetLine(points[i], points[j]));
+        all_lines.push_back(Line(points[i], points[j]));
       }
     }
 
     std::vector<int> count(all_lines.size());
     for (const auto& p : points) {
       for (int i = 0; i < all_lines.size(); ++i) {
-        if (IsOnLine(p, all_lines[i])) {
+        if (IsOnLine(all_lines[i], p)) {
           ++count[i];
         }
       }
