7b3a0785d23a3f575ff41d505ff34e2c77c2fa69
bogdan-lab <lakatosh@phystech.edu>
2022-12-06 08:50:52 +0300

    Done recursive solution

--- Tasks/LC_task_154.cpp
+++ Tasks/LC_task_154.cpp
@@ -1,29 +1,16 @@
 #include <algorithm>
 #include <vector>
 
-int GetMinInRotated(const std::vector<int>& nums, int l, int r) {
-  while (r - l > 1) {
-    int m = (r + l) / 2;
-    if (nums[m] <= nums[r]) {
-      r = m;
-    } else {
-      l = m;
-    }
-  }
-  return std::min(nums[l], nums[r]);
+int GetMinimum(const std::vector<int>& nums, int l, int r) {
+  if (r - l <= 1) return std::min(nums[l], nums[r]);
+  if (nums[l] < nums[r]) return nums[l];
+  int m = (l + r) / 2;
+  return std::min(GetMinimum(nums, l, m), GetMinimum(nums, m + 1, r));
 }
 
 class Solution {
  public:
   int findMin(std::vector<int>& nums) {
-    int r = nums.size() - 1;
-    int l = 0;
-    while (l < r && nums[r] == nums[l]) {
-      ++l;
-    }
-    if (nums[l] > nums[r]) {
-      return GetMinInRotated(nums, l, r);
-    }
-    return nums[l];
+    return GetMinimum(nums, 0, nums.size() - 1);
   }
 };
