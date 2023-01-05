0fe43046b23b45f9a1ee72dfe9b3aac49d5381f1
bogdan-lab <lakatosh@phystech.edu>
2023-01-05 10:34:51 +0300

    Done

--- Tasks/LC_task_174.cpp
+++ Tasks/LC_task_174.cpp
@@ -1,47 +1,32 @@
 #include <algorithm>
 #include <vector>
 
-struct Path {
-  int agg = 0;
-  int min_drop = 0;
-};
-
 class Solution {
  public:
   int calculateMinimumHP(std::vector<std::vector<int>>& dungeon) {
-    std::vector<std::vector<Path>> data(dungeon.size(),
-                                        std::vector<Path>(dungeon[0].size()));
+    int rows = dungeon.size();
+    int cols = dungeon[0].size();
+    std::vector<std::vector<int>> data(rows, std::vector<int>(cols));
 
-    data[0][0].agg = dungeon[0][0];
-    data[0][0].min_drop = dungeon[0][0];
-    for (int i = 1; i < dungeon[0].size(); ++i) {
-      data[0][i].agg = data[0][i - 1].agg + dungeon[0][i];
-      data[0][i].min_drop = std::min(data[0][i - 1].min_drop, data[0][i].agg);
+    data[rows - 1][cols - 1] =
+        dungeon[rows - 1][cols - 1] >= 0 ? 1 : 1 - dungeon[rows - 1][cols - 1];
+
+    for (int i = rows - 2; i >= 0; --i) {
+      data[i][cols - 1] =
+          std::max(data[i + 1][cols - 1] - dungeon[i][cols - 1], 1);
+    }
+    for (int i = cols - 2; i >= 0; --i) {
+      data[rows - 1][i] =
+          std::max(data[rows - 1][i + 1] - dungeon[rows - 1][i], 1);
     }
 
-    for (int i = 1; i < dungeon.size(); ++i) {
-      data[i][0].agg = data[i - 1][0].agg + dungeon[i][0];
-      data[i][0].min_drop = std::min(data[i - 1][0].min_drop, data[i][0].agg);
-    }
-
-    for (int i = 1; i < dungeon.size(); ++i) {
-      for (int j = 1; j < dungeon[0].size(); ++j) {
-        int top_agg = data[i - 1][j].agg + dungeon[i][j];
-        int from_top = std::min(top_agg, data[i - 1][j].min_drop);
-        int right_agg = data[i][j - 1].agg + dungeon[i][j];
-        int from_right = std::min(right_agg, data[i][j - 1].min_drop);
-        if (right_agg > top_agg ||
-            (right_agg == top_agg && from_right > from_top)) {
-          data[i][j].min_drop = from_right;
-          data[i][j].agg = right_agg;
-        } else {
-          data[i][j].min_drop = from_top;
-          data[i][j].agg = top_agg;
-        }
+    for (int i = rows - 2; i >= 0; --i) {
+      for (int j = cols - 2; j >= 0; --j) {
+        data[i][j] = std::max(1, std::min(data[i + 1][j] - dungeon[i][j],
+                                          data[i][j + 1] - dungeon[i][j]));
       }
     }
 
-    int min_drop = data.back().back().min_drop;
-    return min_drop >= 0 ? 1 : 1 - min_drop;
+    return data.front().front();
   }
 };
