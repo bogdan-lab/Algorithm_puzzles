0c5415e275683dbb7d84359cbc7f13e596333eed
bogdan-lab <lakatosh@phystech.edu>
2023-01-13 08:37:24 +0300

    Stack solution - TLE

--- Tasks/LC_task_218.cpp
+++ Tasks/LC_task_218.cpp
@@ -1,70 +1,78 @@
+#include <algorithm>
+#include <cassert>
+#include <limits>
 #include <stack>
 #include <vector>
+
+void ClearStack(std::vector<std::vector<int>>& stk,
+                std::vector<std::vector<int>>& res, int64_t next_left) {
+  while (!stk.empty() && stk.back()[1] <= next_left) {
+    const auto& top = stk.back();
+    if (stk.size() > 1) {
+      const auto& prev = stk[stk.size() - 2];
+      if (prev[1] > top[1]) {
+        res.push_back({top[1], prev[2]});
+      } else {
+        std::swap(stk.back(), stk[stk.size() - 2]);
+        // Here we do nothing and pop in the end of the loop
+        // We need to find the next highest building with edge futher on right.
+      }
+    } else {
+      if (top[1] < next_left) {
+        res.push_back({top[1], 0});
+      }
+    }
+
+    stk.pop_back();
+  }
+}
+
+void ExpandEqualHeights(std::vector<int>& lb, std::vector<int>& rb) {
+  assert(lb[2] == rb[2]);
+  int ml = std::min(lb[0], rb[0]);
+  int mr = std::max(rb[1], lb[1]);
+  lb[0] = rb[0] = ml;
+  lb[1] = rb[1] = mr;
+}
+
+void PushBuilding(std::vector<std::vector<int>>& stk,
+                  const std::vector<int>& b) {
+  stk.push_back(b);
+  if (stk.size() == 1) return;
+  int i = stk.size() - 2;
+  while (i >= 0 && stk[i][2] > b[2]) {
+    std::swap(stk[i], stk[i + 1]);
+    --i;
+  }
+  while (i >= 0 && stk[i][2] == b[2]) {
+    ExpandEqualHeights(stk[i], stk[i + 1]);
+    --i;
+  }
+}
 
 class Solution {
  public:
   std::vector<std::vector<int>> getSkyline(
       std::vector<std::vector<int>>& buildings) {
+    std::vector<std::vector<int>> stk;
     std::vector<std::vector<int>> res;
-
-    std::stack<std::vector<int>> buff;
-    std::stack<std::vector<int>> buff2;
     int i = 0;
     while (i < buildings.size()) {
-      int left = buildings[i][0];
-      // TODO pop all in the past
-      while (left == buildings[i][0]) {
-        if (buff.empty()) {
-          buff.push(buildings[i]);
-        } else if (buff.top()[2] < buildings[i][2]) {
-          if (buff.top()[1] <= buildings[i][1]) {
-            buff.pop();
-          }
-          buff.push(buildings[i]);
-        } else if (buff.top()[2] > buildings[i][2] &&
-                   buildings[i][1] > buff.top()[1]) {
-          while (!buff.empty() && buildings[i][1] > buff.top()[1] &&
-                 buildings[i][2] < buff.top()[2]) {
-            buff2.push(buff.top());
-            buff.pop();
-          }
-          // check why we stopped
-          if (buff.empty()) {
-            buff.push(buildings[i]);
-          } else if (buildings[i][1] <= buff.top()[1]) {
-            if (buildings[i][2] > buff.top()[2]) {
-              if (buildings[i][1] == buff.top()[1]) {
-                buff.pop();
-              }
-              buff.push(buildings[i]);
-            }
-          } else {
-            // buildings[i][2] >= buff.top()[2]
-            if (buildings[i][2] == buff.top()[2]) {
-              if (buildings[i][1] > buff.top()[1]) {
-                buff.pop();
-                buff.push(buildings[i]);
-              }
-            } else {
-              if (!buff.empty() && buildings[i][1] >= buff.top()[1]) {
-                buff.pop();
-              }
-              buff.push(buildings[i]);
-            }
-          }
-
-          if (buff.empty() || !(buildings[i][1] <= buff.top()[1] &&
-                                buildings[i][2] < buff.top()[2])) {
-          }
-        }
-        buff.push(buildings[i]);
+      ClearStack(stk, res, buildings[i][0]);
+      // Here in the stk we have sorted by height buildings, which crossect the
+      // start of current building
+      // Now we push all buildings with current left boarder, while keeping the
+      // stk sorted
+      int initial_left = buildings[i][0];
+      while (i < buildings.size() && buildings[i][0] == initial_left) {
+        PushBuilding(stk, buildings[i]);
         ++i;
       }
-
-      res.push_back({left, buff.top()[2]});
-      ++i;
+      if (res.empty() || res.back()[1] != stk.back()[2]) {
+        res.push_back({initial_left, stk.back()[2]});
+      }
     }
-
+    ClearStack(stk, res, std::numeric_limits<int64_t>::max());
     return res;
   }
 };
