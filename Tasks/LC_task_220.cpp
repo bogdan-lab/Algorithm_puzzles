34ebdc5a415b2c49d5e1cf8d889a950f44df7dc4
bogdan-lab <lakatosh@phystech.edu>
2023-01-14 21:28:43 +0300

    done

--- Tasks/LC_task_220.cpp
+++ Tasks/LC_task_220.cpp
@@ -1,69 +1,37 @@
 #include <algorithm>
 #include <cassert>
 #include <limits>
-#include <unordered_set>
+#include <unordered_map>
 #include <vector>
-
-bool Check(const std::vector<std::pair<int, int>>& levels, int w_size) {
-  std::unordered_set<int> count;
-  int i = 0;
-  while (i < levels.size() && levels[i].first < w_size) {
-    auto it = count.insert(levels[i].second);
-    if (!it.second) return true;
-    ++i;
-  }
-
-  int j = 0;
-  while (i < levels.size()) {
-    // remove tail
-    while (levels[j].first <= levels[i].first - w_size) {
-      count.erase(levels[j].second);
-      ++j;
-    }
-    auto it = count.insert(levels[i].second);
-    if (!it.second) return true;
-    ++i;
-  }
-  return false;
-}
-
-std::vector<std::pair<int, int>> GetLevels(const std::vector<int>& nums,
-                                           int diff) {
-  std::vector<std::pair<int, int>> levels;
-  levels.reserve(2 * nums.size());
-  for (int i = 0; i < nums.size(); ++i) {
-    if (diff == 0) {
-      levels.push_back({i, nums[i]});
-    } else {
-      int val = nums[i] / diff;
-      if (!(nums[i] % diff)) {
-        levels.push_back({i, val - 1});
-        levels.push_back({i, val});
-      } else {
-        levels.push_back({i, val});
-      }
-    }
-  }
-  return levels;
-}
 
 class Solution {
  public:
   bool containsNearbyAlmostDuplicate(std::vector<int>& nums, int indexDiff,
                                      int valueDiff) {
-    assert(!nums.empty());
-    int min_val = *std::min_element(nums.begin(), nums.end());
+    std::unordered_map<int, int> count;
+    for (int i = 0; i < nums.size(); ++i) {
+      int lvl = nums[i] / (valueDiff + 1);
+      if (nums[i] < 0) --lvl;
+      auto it = count.find(lvl);
+      if (it != count.end()) return true;
+      count[lvl] = nums[i];
 
-    if (valueDiff > 0) {
-      int tmp = (std::abs(min_val) + valueDiff - 1) / valueDiff;
-      min_val = tmp * valueDiff;
+      it = count.find(lvl - 1);
+      if (it != count.end() && std::abs(it->second - nums[i]) <= valueDiff)
+        return true;
+
+      it = count.find(lvl + 1);
+      if (it != count.end() && std::abs(it->second - nums[i]) <= valueDiff)
+        return true;
+
+      if (count.size() > indexDiff) {
+        int prev = i - indexDiff;
+        int prev_lvl = nums[prev] / (valueDiff + 1);
+        if (nums[prev] < 0) --prev_lvl;
+        count.erase(prev_lvl);
+      }
     }
 
-    for (auto& el : nums) {
-      el += min_val;
-    }
-
-    std::vector<std::pair<int, int>> levels = GetLevels(nums, valueDiff);
-    return Check(levels, std::min<int>(indexDiff + 1, nums.size()));
+    return false;
   }
 };
