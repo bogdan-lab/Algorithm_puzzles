015264037ed65161470e3cd019744cab8f02f726
bogdan-lab <lakatosh@phystech.edu>
2023-01-14 09:24:50 +0300

    Pure Graham does not work

--- Tasks/LC_task_218.cpp
+++ Tasks/LC_task_218.cpp
@@ -2,77 +2,101 @@
 #include <cassert>
 #include <limits>
 #include <stack>
+#include <tuple>
 #include <vector>
 
-void ClearStack(std::vector<std::vector<int>>& stk,
-                std::vector<std::vector<int>>& res, int64_t next_left) {
-  while (!stk.empty() && stk.back()[1] <= next_left) {
-    const auto& top = stk.back();
-    if (stk.size() > 1) {
-      const auto& prev = stk[stk.size() - 2];
-      if (prev[1] > top[1]) {
-        res.push_back({top[1], prev[2]});
-      } else {
-        std::swap(stk.back(), stk[stk.size() - 2]);
-        // Here we do nothing and pop in the end of the loop
-        // We need to find the next highest building with edge futher on right.
-      }
+struct Point {
+  int64_t x = 0;
+  int64_t y = 0;
+};
+
+enum class Direction { kLeft, kRight, kStraight };
+
+Direction GetDirection(Point p0, Point p1, Point p2) {
+  Point lhs{p1.x - p0.x, p1.y - p0.y};
+  Point rhs{p2.x - p0.x, p2.y - p0.y};
+
+  int64_t r = lhs.y * rhs.x;
+  int64_t l = lhs.x * rhs.y;
+
+  if (l > r) {
+    return Direction::kLeft;
+  } else if (l < r) {
+    return Direction::kRight;
+  } else {
+    return Direction::kStraight;
+  }
+}
+
+std::vector<Point> GrahamScan(const std::vector<Point>& data) {
+  std::vector<Point> res;
+  res.push_back(data[0]);
+  res.push_back(data[1]);
+  res.push_back(data[2]);
+
+  for (int i = 3; i < data.size(); ++i) {
+    while (GetDirection(res[res.size() - 2], res.back(), data[i]) ==
+           Direction::kLeft) {
+      res.pop_back();
+    }
+    res.push_back(data[i]);
+  }
+
+  return res;
+}
+
+std::vector<std::vector<int>> ExtractAnswer(const std::vector<Point>& gstk) {
+  std::vector<std::vector<int>> res;
+  res.reserve(gstk.size() - 1);
+  for (int i = 1; i < gstk.size(); ++i) {
+    res.emplace_back();
+    if (res.size() == 1) {
+      res.back().push_back(gstk[i].x);
+      res.back().push_back(gstk[i].y);
     } else {
-      if (top[1] < next_left) {
-        res.push_back({top[1], 0});
+      if (res[res.size() - 2][1] > gstk[i].y) {
+        res.back().push_back(res[res.size() - 2][0]);
+        res.back().push_back(gstk[i].y);
+      } else if (res[res.size() - 2][1] < gstk[i].y) {
+        res.back().push_back(gstk[i].x);
+        res.back().push_back(gstk[i].y);
       }
     }
-
-    stk.pop_back();
   }
-}
 
-void ExpandEqualHeights(std::vector<int>& lb, std::vector<int>& rb) {
-  assert(lb[2] == rb[2]);
-  int ml = std::min(lb[0], rb[0]);
-  int mr = std::max(rb[1], lb[1]);
-  lb[0] = rb[0] = ml;
-  lb[1] = rb[1] = mr;
-}
-
-void PushBuilding(std::vector<std::vector<int>>& stk,
-                  const std::vector<int>& b) {
-  stk.push_back(b);
-  if (stk.size() == 1) return;
-  int i = stk.size() - 2;
-  while (i >= 0 && stk[i][2] > b[2]) {
-    std::swap(stk[i], stk[i + 1]);
-    --i;
-  }
-  while (i >= 0 && stk[i][2] == b[2]) {
-    ExpandEqualHeights(stk[i], stk[i + 1]);
-    --i;
-  }
+  return res;
 }
 
 class Solution {
  public:
   std::vector<std::vector<int>> getSkyline(
       std::vector<std::vector<int>>& buildings) {
-    std::vector<std::vector<int>> stk;
-    std::vector<std::vector<int>> res;
-    int i = 0;
-    while (i < buildings.size()) {
-      ClearStack(stk, res, buildings[i][0]);
-      // Here in the stk we have sorted by height buildings, which crossect the
-      // start of current building
-      // Now we push all buildings with current left boarder, while keeping the
-      // stk sorted
-      int initial_left = buildings[i][0];
-      while (i < buildings.size() && buildings[i][0] == initial_left) {
-        PushBuilding(stk, buildings[i]);
-        ++i;
-      }
-      if (res.empty() || res.back()[1] != stk.back()[2]) {
-        res.push_back({initial_left, stk.back()[2]});
-      }
+    std::vector<Point> data;
+    data.reserve(2 * buildings.size() + 2);
+    data.push_back({buildings[0][0], 0});
+    data.push_back({std::numeric_limits<int>::max(), 0});
+    for (const auto& el : buildings) {
+      data.push_back({el[0], el[2]});
+      data.push_back({el[1], el[2]});
     }
-    ClearStack(stk, res, std::numeric_limits<int64_t>::max());
-    return res;
+
+    std::sort(data.begin(), data.end(), [](Point l, Point r) {
+      return std::tie(l.x, l.y) < std::tie(r.x, r.y);
+    });
+
+    std::vector<Point> gstk = GrahamScan(data);
+    return ExtractAnswer(gstk);
   }
 };
+
+int main() {
+  Solution s;
+  {
+    std::vector<std::vector<int>> input{
+        {2, 9, 10}, {3, 7, 15}, {5, 12, 12}, {15, 20, 10}, {19, 24, 8}};
+    std::vector<std::vector<int>> exp{{2, 10},  {3, 15}, {7, 12}, {12, 0},
+                                      {15, 10}, {20, 8}, {24, 0}};
+    assert(s.getSkyline(input) == exp);
+  }
+  return 0;
+}
