35f0b4e720c82eb3a7fa0a76c5ae6dc9238f94bb
bogdan-lab <lakatosh@phystech.edu>
2023-01-22 16:59:46 +0300

    Need to add multidigit multiplication

--- Tasks/LC_task_282.cpp
+++ Tasks/LC_task_282.cpp
@@ -1,89 +1,73 @@
 #include <algorithm>
 #include <iostream>
 #include <string>
-#include <string_view>
 #include <vector>
 
-void BuildPathes(std::string_view str, int64_t target, std::string& curr_path,
-                 std::vector<std::string>& res);
-void TryPlusMinus(int64_t curr_val, int64_t target, std::string_view next_str,
-                  std::string& curr_path, std::vector<std::string>& res);
-void TrySingleValue(std::string_view str, int64_t target,
-                    std::string& curr_path, std::vector<std::string>& res);
-void TryMultiplication(std::string_view str, int64_t target,
-                       std::string& curr_path, std::vector<std::string>& res);
-void TryManyDigits(std::string_view str, int64_t target, std::string& curr_path,
+int64_t Apply(int64_t l, int64_t r, char op);
+void TrySingleVal(char op, const std::string& str, int i, int64_t curr_val,
+                  int64_t target, std::string& curr_path,
+                  std::vector<std::string>& res);
+void TryMultiplied(char op, const std::string& str, int i, int64_t curr_val,
+                   int64_t target, std::string& curr_path,
                    std::vector<std::string>& res);
+void TryMultiDigit(char op, const std::string& str, int i, int64_t curr_val,
+                   int64_t target, std::string& curr_path,
+                   std::vector<std::string>& res);
+void TryOperation(char op, const std::string& str, int i, int64_t curr_val,
+                  int64_t target, std::string& curr_path,
+                  std::vector<std::string>& res);
+void BuildPath(const std::string& str, int i, int64_t curr_val, int64_t target,
+               std::string& curr_path, std::vector<std::string>& res);
 
 int64_t ToInt(char c) { return static_cast<int>(c) - static_cast<int>('0'); }
 
-void TryPlusMinus(int64_t curr_val, int64_t target, std::string_view next_str,
-                  std::string& curr_path, std::vector<std::string>& res) {
-  // Try this value with +
-  curr_path.push_back('+');
-  BuildPathes(next_str, target - curr_val, curr_path, res);
-  curr_path.pop_back();
-  // Try this value with -
-  curr_path.push_back('-');
-  BuildPathes(next_str, target + curr_val, curr_path, res);
-  curr_path.pop_back();
+int64_t Apply(int64_t l, int64_t r, char op) {
+  if (op == '+') return l + r;
+  return l - r;
 }
 
-void TrySingleValue(std::string_view str, int64_t target,
-                    std::string& curr_path, std::vector<std::string>& res) {
-  curr_path.push_back(str.front());
-  int64_t curr_val = ToInt(str.front());
-  if (str.size() == 1) {
-    if (curr_val == target) {
-      res.push_back(curr_path);
-    }
-  } else {
-    TryPlusMinus(curr_val, target, str.substr(1), curr_path, res);
-  }
+void TrySingleVal(char op, const std::string& str, int i, int64_t curr_val,
+                  int64_t target, std::string& curr_path,
+                  std::vector<std::string>& res) {
+  curr_path.push_back(str[i]);
+  BuildPath(str, i + 1, Apply(curr_val, ToInt(str[i]), op), target, curr_path,
+            res);
   curr_path.pop_back();
 }
 
-void TryMultiplication(std::string_view str, int64_t target,
-                       std::string& curr_path, std::vector<std::string>& res) {
-  if (str.size() < 2) return;
-  int initial_size = curr_path.size();
-  curr_path.push_back(str.front());
-  int64_t curr_val = ToInt(str.front());
-  for (int i = 1; i < str.size() - 1; ++i) {
-    curr_path.push_back('*');
-    curr_path.push_back(str[i]);
-    curr_val *= ToInt(str[i]);
-    TryPlusMinus(curr_val, target, str.substr(i + 1), curr_path, res);
-  }
-  // the last value is treated separately
-  curr_path.push_back('*');
-  curr_path.push_back(str.back());
-  curr_val *= ToInt(str.back());
-  if (target == curr_val) {
-    res.push_back(curr_path);
-  }
-  while (curr_path.size() > initial_size) {
-    curr_path.pop_back();
-  }
-}
-
-void TryManyDigits(std::string_view str, int64_t target, std::string& curr_path,
+void TryMultiplied(char op, const std::string& str, int i, int64_t curr_val,
+                   int64_t target, std::string& curr_path,
                    std::vector<std::string>& res) {
-  if (str.size() < 2 || str.front() == '0') return;
-  int initial_size = curr_path.size();
+  if (i + 1 >= str.size()) return;
 
-  int64_t curr_val = ToInt(str.front());
-  curr_path.push_back(str.front());
-  for (int i = 1; i < str.size() - 1; ++i) {
-    curr_path.push_back(str[i]);
-    curr_val = 10 * curr_val + ToInt(str[i]);
-    TryPlusMinus(curr_val, target, str.substr(i + 1), curr_path, res);
+  int initial_size = curr_path.size();
+  curr_path.push_back(str[i]);
+  int64_t mult_val = ToInt(str[i]);
+  for (int j = i + 1; j < str.size(); ++j) {
+    curr_path.push_back('*');
+    curr_path.push_back(str[j]);
+    mult_val *= ToInt(str[j]);
+    BuildPath(str, j + 1, Apply(curr_val, mult_val, op), target, curr_path,
+              res);
   }
-  // The last case separately
-  curr_path.push_back(str.back());
-  curr_val = 10 * curr_val + ToInt(str.back());
-  if (curr_val == target) {
-    res.push_back(curr_path);
+
+  while (initial_size < curr_path.size()) {
+    curr_path.pop_back();
+  }
+}
+
+void TryMultiDigit(char op, const std::string& str, int i, int64_t curr_val,
+                   int64_t target, std::string& curr_path,
+                   std::vector<std::string>& res) {
+  if (i + 1 >= str.size() || str[i] == '0') return;
+
+  int initial_size = curr_path.size();
+  curr_path.push_back(str[i]);
+  int64_t md_val = ToInt(str[i]);
+  for (int j = i + 1; j < str.size(); ++j) {
+    curr_path.push_back(str[j]);
+    md_val = 10 * md_val + ToInt(str[j]);
+    BuildPath(str, j + 1, Apply(curr_val, md_val, op), target, curr_path, res);
   }
 
   while (curr_path.size() > initial_size) {
@@ -91,11 +75,27 @@ void TryManyDigits(std::string_view str, int64_t target, std::string& curr_path,
   }
 }
 
-void BuildPathes(std::string_view str, int64_t target, std::string& curr_path,
-                 std::vector<std::string>& res) {
-  TrySingleValue(str, target, curr_path, res);
-  TryMultiplication(str, target, curr_path, res);
-  TryManyDigits(str, target, curr_path, res);
+void TryOperation(char op, const std::string& str, int i, int64_t curr_val,
+                  int64_t target, std::string& curr_path,
+                  std::vector<std::string>& res) {
+  curr_path.push_back(op);
+  TrySingleVal(op, str, i, curr_val, target, curr_path, res);
+  TryMultiplied(op, str, i, curr_val, target, curr_path, res);
+  TryMultiDigit(op, str, i, curr_val, target, curr_path, res);
+  curr_path.pop_back();
+}
+
+void BuildPath(const std::string& str, int i, int64_t curr_val, int64_t target,
+               std::string& curr_path, std::vector<std::string>& res) {
+  std::cout << curr_path << " = " << curr_val << '\n';
+  if (i == str.size()) {
+    if (curr_val == target) {
+      res.push_back(curr_path.substr(1));
+    }
+    return;
+  }
+  TryOperation('+', str, i, curr_val, target, curr_path, res);
+  TryOperation('-', str, i, curr_val, target, curr_path, res);
 }
 
 class Solution {
@@ -103,8 +103,8 @@ class Solution {
   std::vector<std::string> addOperators(std::string num, int target) {
     std::vector<std::string> res;
     std::string curr_path;
-    curr_path.reserve(2 * num.size() - 1);
-    BuildPathes(num, target, curr_path, res);
+    curr_path.reserve(2 * num.size());
+    TryOperation('+', num, 0, 0, target, curr_path, res);
     return res;
   }
 };
@@ -112,7 +112,7 @@ class Solution {
 int main() {
   {
     Solution s;
-    std::vector<std::string> data = s.addOperators("232", 8);
+    std::vector<std::string> data = s.addOperators("2222222", 0);
     for (const auto& el : data) {
       std::cout << el << '\n';
     }
