03674300b9044af1e45e33ae5b906cd9eafb81c4
bogdan-lab <lakatosh@phystech.edu>
2023-02-01 23:32:59 +0300

    Remove redundant memory usage

--- Tasks/LC_task_312.cpp
+++ Tasks/LC_task_312.cpp
@@ -6,13 +6,19 @@ int GetVal(const std::vector<int>& nums, int i) {
   return nums[i];
 }
 
+int GetDP(const std::vector<std::vector<int>>& nums, int l, int r) {
+  if (l > r) return 0;
+  return nums[l][r];
+}
+
 int CalcMaxPoints(const std::vector<int>& nums, int start, int end,
                   const std::vector<std::vector<int>>& dp) {
   int res = 0;
-  for (int k = start; k < end; ++k) {
+  for (int k = start; k <= end; ++k) {
     int last_tick =
-        GetVal(nums, start - 1) * GetVal(nums, k) * GetVal(nums, end);
-    res = std::max(res, last_tick + dp[start][k] + dp[k + 1][end]);
+        GetVal(nums, start - 1) * GetVal(nums, k) * GetVal(nums, end + 1);
+    res = std::max(res,
+                   last_tick + GetDP(dp, start, k - 1) + GetDP(dp, k + 1, end));
   }
   return res;
 }
@@ -21,16 +27,16 @@ class Solution {
  public:
   int maxCoins(std::vector<int>& nums) {
     // dp[start][end]
-    std::vector<std::vector<int>> dp(nums.size() + 1,
-                                     std::vector<int>(nums.size() + 1));
+    std::vector<std::vector<int>> dp(nums.size(),
+                                     std::vector<int>(nums.size()));
 
-    for (int w = 1; w <= nums.size(); ++w) {
-      for (int start = 0; start + w <= nums.size(); ++start) {
+    for (int w = 0; w < nums.size(); ++w) {
+      for (int start = 0; start + w < nums.size(); ++start) {
         int end = start + w;
         dp[start][end] = CalcMaxPoints(nums, start, end, dp);
       }
     }
 
-    return dp[0][nums.size()];
+    return dp[0][nums.size() - 1];
   }
 };
