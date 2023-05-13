8abd3c739cde6246ffa27bf9c5f2038c98c67f73
bogdan-lab <lakatosh.b.v@gmail.com>
2023-05-13 19:41:40 +0200

    Done

--- Tasks/LC_task_321.cpp
+++ Tasks/LC_task_321.cpp
@@ -1,7 +1,4 @@
-#include <algorithm>
 #include <cassert>
-#include <iostream>
-#include <limits>
 #include <vector>
 
 using Number = std::vector<int>;
@@ -9,13 +6,6 @@ struct EqualRes {
   size_t count = 0;
   bool left_is_better = true;
 };
-
-void PrintNumber(const Number& n) {
-  for (const auto& el : n) {
-    std::cout << el << ",";
-  }
-  std::cout << '\n';
-}
 
 bool IsLess(const std::vector<int>& lhs, const std::vector<int>& rhs) {
   assert(lhs.size() == rhs.size());
@@ -37,9 +27,9 @@ Number Create(int prefix, const Number& suffix) {
   return res;
 }
 
-Number BuildMaxNumber(const Number& data, size_t num_size) {
+void BuildMaxNumber(const Number& data, size_t num_size, Number& res) {
   assert(num_size <= data.size());
-  Number res;
+  res.clear();
   res.reserve(num_size);
   for (size_t i = 0; i < data.size(); ++i) {
     size_t elements_left = data.size() - i - 1;
@@ -52,7 +42,6 @@ Number BuildMaxNumber(const Number& data, size_t num_size) {
     }
   }
   assert(res.size() == num_size);
-  return res;
 }
 
 EqualRes CountEqual(const Number& lhs, size_t li, const Number& rhs,
@@ -60,8 +49,12 @@ EqualRes CountEqual(const Number& lhs, size_t li, const Number& rhs,
   EqualRes res;
   assert(lhs[li] == rhs[ri]);
   auto val = lhs[li];
+  bool is_in_const_layer = true;
   while (li < lhs.size() && ri < rhs.size() && lhs[li] == rhs[ri]) {
-    res.count += (lhs[li] == val);
+    if (is_in_const_layer && lhs[li] != val) {
+      is_in_const_layer = false;
+    }
+    res.count += is_in_const_layer;
     ++li;
     ++ri;
   }
@@ -71,8 +64,8 @@ EqualRes CountEqual(const Number& lhs, size_t li, const Number& rhs,
   return res;
 }
 
-Number Merge(const Number& lhs, const Number& rhs) {
-  Number res;
+void Merge(const Number& lhs, const Number& rhs, Number& res) {
+  res.clear();
   res.reserve(lhs.size() + rhs.size());
   size_t li = 0;
   size_t ri = 0;
@@ -98,32 +91,24 @@ Number Merge(const Number& lhs, const Number& rhs) {
       }
     }
   }
-  return res;
 }
 
 Number FindLargestNumber(Number& lhs, Number& rhs, int k) {
   Number max_num(k, 0);
-  std::cout << "lhs.size = " << lhs.size() << " rhs.size = " << rhs.size()
-            << '\n';
+  Number lhs_best;
+  Number rhs_best;
+  Number current;
+  current.reserve(k);
   for (size_t left_size = 0; left_size <= k; ++left_size) {
     size_t right_size = k - left_size;
     if (left_size > lhs.size() || right_size > rhs.size()) {
       continue;
     }
-    Number lhs_best = BuildMaxNumber(lhs, left_size);
-    Number rhs_best = BuildMaxNumber(rhs, right_size);
-    // std::cout << "lbest_size = " << lhs_best.size()
-    //           << " rbest_size = " << rhs_best.size() << '\n';
-    Number current = Merge(lhs_best, rhs_best);
-    std::cout << "LEFT = ";
-    PrintNumber(lhs_best);
-    std::cout << "RIGHT = ";
-    PrintNumber(rhs_best);
-    PrintNumber(current);
+    BuildMaxNumber(lhs, left_size, lhs_best);
+    BuildMaxNumber(rhs, right_size, rhs_best);
+    Merge(lhs_best, rhs_best, current);
     if (IsLess(max_num, current)) {
-      max_num = std::move(current);
-      // std::cout << "MAX_NUM = \n";
-      // PrintNumber(max_num);
+      max_num.swap(current);
     }
   }
   return max_num;
@@ -138,9 +123,30 @@ class Solution {
 
 int main() {
   Solution s;
-  Number left{2, 1, 7, 8, 0, 1, 7, 3, 5, 8, 9, 0, 0, 7, 0, 2, 2, 7, 3, 5, 5};
-  Number right{2, 6, 2, 0, 1, 0, 5, 4, 5, 5, 3, 3, 3, 4};
-  auto res = s.maxNumber(left, right, 35);
-  PrintNumber(res);
+  Number left{2, 1, 2, 1, 2, 2, 1, 2, 2, 1, 1, 2, 1, 0, 2, 0, 1, 0, 1, 1, 2,
+              0, 0, 2, 2, 2, 2, 1, 1, 1, 2, 1, 2, 0, 2, 0, 1, 1, 0, 1, 0, 2,
+              0, 1, 0, 2, 0, 1, 1, 0, 0, 2, 0, 1, 1, 2, 0, 2, 2, 1, 2, 1, 2,
+              1, 0, 1, 2, 0, 2, 1, 2, 2, 2, 0, 1, 1, 0, 2, 0, 1, 1, 0, 0, 0,
+              2, 1, 1, 1, 0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 2, 1, 2, 2, 1, 1,
+              0, 1, 1, 0, 0, 1, 0, 0, 0, 2, 1, 1, 0, 2, 0, 2, 2, 0, 2, 0, 0,
+              2, 0, 1, 2, 1, 1, 1, 2, 1, 0, 1, 1, 0, 2, 1, 2, 2, 1, 0, 1, 1,
+              1, 2, 0, 2, 2, 2, 0, 2, 1, 1, 2, 1, 1, 2, 0, 2, 1, 0, 2, 0, 0,
+              2, 2, 2, 0, 2, 1, 2, 2, 1, 2, 1, 2, 2, 2, 1, 1, 2, 0, 2, 0, 0,
+              2, 2, 2, 0, 2, 2, 1, 0, 0, 2, 2, 2, 1, 1, 0, 2, 1, 0, 1, 2, 1,
+              1, 2, 2, 1, 1, 0, 2, 1, 2, 2, 1, 2, 1, 0, 0, 0, 0, 1, 1, 0, 2,
+              2, 2, 2, 2, 2, 2, 2, 1, 1, 0, 2, 1, 0, 0, 0, 0, 2, 1, 1};
+  Number right{1, 1, 0, 2, 0, 1, 1, 1, 0, 2, 2, 2, 1, 1, 0, 1, 2, 1, 2, 1, 0,
+               1, 2, 2, 2, 2, 1, 1, 0, 2, 0, 1, 0, 0, 1, 1, 0, 1, 2, 1, 2, 1,
+               2, 0, 1, 1, 1, 1, 2, 0, 1, 1, 1, 0, 0, 1, 0, 1, 2, 1, 1, 0, 2,
+               2, 1, 2, 0, 2, 0, 1, 1, 2, 0, 1, 1, 2, 2, 1, 0, 1, 2, 2, 0, 1,
+               1, 2, 2, 0, 2, 2, 0, 2, 1, 0, 0, 2, 1, 0, 0, 2, 1, 2, 1, 2, 0,
+               2, 0, 1, 1, 2, 1, 1, 1, 2, 0, 2, 2, 0, 2, 2, 0, 2, 1, 2, 1, 2,
+               0, 2, 0, 0, 1, 2, 2, 2, 2, 1, 2, 2, 0, 1, 0, 0, 2, 2, 2, 2, 0,
+               1, 0, 2, 1, 2, 2, 2, 1, 1, 1, 1, 2, 0, 0, 1, 0, 0, 2, 2, 1, 0,
+               0, 1, 1, 0, 0, 1, 1, 0, 2, 2, 2, 2, 2, 1, 0, 2, 2, 0, 0, 1, 0,
+               0, 1, 1, 1, 2, 2, 0, 0, 2, 0, 0, 0, 1, 2, 0, 2, 0, 1, 2, 0, 1,
+               2, 0, 1, 1, 0, 0, 1, 2, 1, 0, 2, 1, 0, 1, 2, 0, 1, 1, 2, 1, 0,
+               2, 1, 2, 1, 1, 0, 2, 2, 1, 0, 2, 1, 1, 1, 0, 0, 0, 1, 0};
+  s.maxNumber(left, right, 500);
   return 0;
 }
